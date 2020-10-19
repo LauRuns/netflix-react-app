856daaaa9136c7421ad6ccd27caabc2c8eae80a1
Andy <aste.rix@icloud.com>
2020-10-19 18:07:26 +0200

    adds logo component to navbar

--- src/shared/components/Navigation/Navbar/Navbar.js
+++ src/shared/components/Navigation/Navbar/Navbar.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import NavItems from '../NavItems/NavItems';
+import Logo from '../../../../components/Logo/Logo';
 import './Navbar.css';
 
 const Navbar = props => {
     return (
         <header className="Navbar">
+            <div className="NavbarLogo">
+                <Logo />
+            </div>
             <nav className="DesktopOnly">
                 <NavItems />
             </nav>
