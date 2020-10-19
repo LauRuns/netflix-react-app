1bae625295ec46703aa63e54356c1071bfb62b6b
Andy <aste.rix@icloud.com>
2020-10-19 17:59:30 +0200

    add netflix app functionality

---
+++ src/containers/NetflixApp/NetflixApp.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const NetflixApp = (props) => {
+    return (
+        <React.Fragment>
+            <h2>Netflix App working</h2>
+            <p>Lorem ipsum dolor sit amet.</p>
+        </React.Fragment>
+    )
+}
+
+
+
+export default NetflixApp;
