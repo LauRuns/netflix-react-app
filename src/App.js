e4d989a7219c772689cd27b3126d563f50abc86c
Andy <aste.rix@icloud.com>
2020-10-19 17:57:48 +0200

    routing setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import {
+  Route,
+  // Redirect,
+  Switch
+} from 'react-router-dom';
+
+import NetflixApp from './containers/NetflixApp/NetflixApp';
+import Account from './containers/Account/Account';
+import Layout from './containers/layouts/Layout';
 
 import './App.css';
 
 const App = () => {
+  // const { token, login, logout, userId } = useAuth();
+
+  let routes;
+
+  // use this with token verification
+  if (true) {
+    routes = (
+      <Switch>
+        <Route path="/" exact component={NetflixApp} />
+        <Route path="/account" exact component={Account} />
+      </Switch>
+    )
+  } else {
+    routes = (
+      <Switch>
+        <Route path="/" exact component={NetflixApp} />
+      </Switch>
+    )
+  }
+
+
   return (
-    <div className="App">
-      <h1>hope this works</h1>
-    </div>
+    <Layout>
+      {routes}
+    </Layout>
   );
 }
 
