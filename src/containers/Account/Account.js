a02d07a7598659c7970e0d48686dd7614f46eb8b
Andy <aste.rix@icloud.com>
2020-10-19 17:58:42 +0200

    add account component

---
+++ src/containers/Account/Account.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const userAccount = (props) => {
+    return (
+        <React.Fragment>
+            <h2>User account works</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, fugit!</p>
+        </React.Fragment>
+    );
+};
+
+
+
+export default userAccount;
