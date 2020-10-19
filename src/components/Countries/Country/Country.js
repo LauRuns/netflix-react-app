8bf8b7323ad9bdf303f0af25c281cd797e57ba51
Andy <aste.rix@icloud.com>
2020-10-19 18:43:23 +0200

    adds country component

---
+++ src/components/Countries/Country/Country.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Card from '../../../shared/components/UIElements/Card/Card';
+import './Country.css';
+
+const country = (props) => {
+    return (
+        <li>
+            <Card>
+                <h3>This is the country page working</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla, modi.</p>
+            </Card>
+        </li>
+    );
+};
+
+
+export default country;
