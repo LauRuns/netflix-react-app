c8cab468260c4517c5776f7b8e7f24bb8801fb94
Andy <aste.rix@icloud.com>
2020-10-19 18:00:25 +0200

    adds navitem using Navlink

---
+++ src/shared/components/Navigation/NavItems/NavItem/NavItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './NavItem.css';
+
+
+const navItem = (props) => {
+    return (
+        <li className="NavItem">
+            <NavLink
+                to={props.link}
+                exact
+                activeClassName="active">{props.children}</NavLink>
+        </li>
+    );
+};
+
+
+
+export default navItem;
