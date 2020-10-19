cc16c2bbe8662058a8e8ce211e1e31ea7df09ccc
Andy <aste.rix@icloud.com>
2020-10-19 18:43:36 +0200

    adds countrylist pcomponentn to app

--- src/containers/NetflixApp/NetflixApp.js
+++ src/containers/NetflixApp/NetflixApp.js
@@ -1,15 +1,16 @@
 import React from 'react'
 
+import Countrylist from '../../components/Countries/CountryList';
 
 const NetflixApp = (props) => {
     return (
         <React.Fragment>
             <h2>Netflix App working</h2>
             <p>Lorem ipsum dolor sit amet.</p>
+            <Countrylist />
         </React.Fragment>
-    )
-}
-
+    );
+};
 
 
 export default NetflixApp;
