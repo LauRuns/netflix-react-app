995c711edfa0c4b97975a84e8bc55eb319f1b024
Andy <aste.rix@icloud.com>
2020-10-20 09:02:34 +0200

    fetches available country data

--- src/containers/NetflixApp/NetflixApp.js
+++ src/containers/NetflixApp/NetflixApp.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-import Countrylist from '../../components/Countries/CountryList';
+import CountryList from '../../components/Countries/CountryList';
+import { useHttpClient } from '../../shared/hooks/http-hook';
 
 const NetflixApp = (props) => {
+    const { isLoading, error, sendRequest, clearError } = useHttpClient();
+    const [loadedCountries, setLoadedCountries] = useState();
+
+
+    useEffect(() => {
+        const fetchCountries = async () => {
+            try {
+                const responseData = await sendRequest(`${process.env.REACT_APP_CONNECTION_STRING}/netflix/countries`);
+                setLoadedCountries(responseData.results);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchCountries();
+    }, [sendRequest]);
+
     return (
         <React.Fragment>
-            <h2>Netflix App working</h2>
-            <p>Lorem ipsum dolor sit amet.</p>
-            <Countrylist />
+            {loadedCountries && <CountryList items={loadedCountries} />}
         </React.Fragment>
     );
 };
