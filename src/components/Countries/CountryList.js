326ff5588786b0587d9cee510ef9f8e793720a73
Andy <aste.rix@icloud.com>
2020-10-20 09:01:09 +0200

    adds countrylist functionality and styling

--- src/components/Countries/CountryList.js
+++ src/components/Countries/CountryList.js
@@ -1,14 +1,29 @@
 import React from 'react';
 
 import Country from './Country/Country';
+import Card from '../../shared/components/UIElements/Card/Card';
 import './CountryList.css';
 
 
-const CountryList = props => {
+const CountryList = (props) => {
+
+    if (props.items.length === 0) {
+        return (
+            <div>
+                <Card>
+                    <h2>No countries found</h2>
+                </Card>
+            </div>
+        );
+    };
+
     return (
         <React.Fragment>
+            <h2 className="CountryList-title">This app has Netflix data available for {props.items.length} countries.</h2>
             <ul className="CountryList">
-                <Country />
+                {props.items.map(country => {
+                    return <Country key={country.countryId} country={country.country} />
+                })}
             </ul>
         </React.Fragment>
 
