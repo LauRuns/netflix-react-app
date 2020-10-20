ff8617aac6068cd731fc03f662825697a721cc93
Andy <aste.rix@icloud.com>
2020-10-20 11:37:33 +0200

    adds opening and closing of sidedrawer

--- src/containers/layouts/Layout.js
+++ src/containers/layouts/Layout.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import Navbar from '../../shared/components/Navigation/Navbar/Navbar';
+import React, { useState } from 'react';
 
+import Navbar from '../../shared/components/Navigation/Navbar/Navbar';
+import SideDrawer from '../../shared/components/Navigation/SideDrawer/SideDrawer';
+import Backdrop from '../../shared/components/UIElements/Backdrop/Backdrop';
 import './Layout.css';
 
 
 const Layout = props => {
+
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+    const openDrawerHandler = () => {
+        setDrawerIsOpen(true);
+    };
+
+    const closeDrawerHandler = () => {
+        setDrawerIsOpen(!drawerIsOpen);
+    };
+
     return (
         <React.Fragment>
-            <Navbar />
+            {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+            <Navbar drawerToggleClicked={openDrawerHandler} />
+            <SideDrawer open={drawerIsOpen} navItemClicked={closeDrawerHandler} />
             {<main className="Content">{props.children}</main>}
         </React.Fragment>
     );
