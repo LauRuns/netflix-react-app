e6fbd09829cddcbe018283252fb3adbfec006e35
Andy <aste.rix@icloud.com>
2020-10-20 11:38:03 +0200

    adds drawer toggle to navbar

--- src/shared/components/Navigation/Navbar/Navbar.js
+++ src/shared/components/Navigation/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import NavItems from '../NavItems/NavItems';
 import Logo from '../../../../components/Logo/Logo';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 import './Navbar.css';
 
 const Navbar = props => {
@@ -13,6 +14,9 @@ const Navbar = props => {
             <nav className="DesktopOnly">
                 <NavItems />
             </nav>
+            <menu className="MobileOnly">
+                <DrawerToggle clicked={props.drawerToggleClicked} />
+            </menu>
         </header>
     )
 }
