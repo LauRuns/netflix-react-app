4a95d5d1ccb60f7e76afd42dac452f5a72e2ea1c
Andy <aste.rix@icloud.com>
2020-10-24 13:28:54 +0200

    adds css grid to account page

--- src/containers/Account/Account.js
+++ src/containers/Account/Account.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
+import './Account.css';
 
 const userAccount = (props) => {
     return (
         <React.Fragment>
-            <h2>User account works</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, fugit!</p>
+            <div className="container">
+                <div className="item" id="item-1">
+                    <h2>User account works</h2>
+
+                </div>
+                <div className="item" id="item-2">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, fugit!</p>
+
+                </div>
+                <div className="item" id="item-3">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Ipsum nisi accusantium asperiores reprehenderit debitis cum placeat,
+                    quia maiores hic nesciunt eligendi totam incidunt cumque similique
+                    expedita soluta officia quisquam sunt ipsa nemo quam? In minima rem aut
+                    expedita aliquid voluptas totam harum distinctio provident,
+                          error nostrum odit earum consectetur nulla! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum minima illo pariatur ipsum exercitationem fugiat, reiciendis veniam voluptas tenetur! Eius accusantium, ducimus atque exercitationem expedita quibusdam, repellendus obcaecati earum beatae nam nesciunt omnis? Cum rem soluta quae sit aperiam dolores, fugiat praesentium porro dolorum nam doloribus? Tempora quae commodi deleniti.</p>
+
+                </div>
+
+            </div>
         </React.Fragment>
     );
 };
