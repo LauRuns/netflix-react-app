3cac0b5c7406419cb58c9bf6e2d0b88ca7de8679
Andy <aste.rix@icloud.com>
2020-10-26 20:03:04 +0100

    adds country stter component

---
+++ src/components/AccountComponents/CountrySetter/CountrySetter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CountrySetter = props => {
+    return (
+        <div>
+            <h3>Countrysetter works</h3>
+        </div>
+    );
+};
+
+
+export default CountrySetter;
