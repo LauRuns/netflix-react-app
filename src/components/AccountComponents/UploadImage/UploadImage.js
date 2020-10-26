110a52db05d7948aea23283c07873d04143384ba
Andy <aste.rix@icloud.com>
2020-10-26 20:03:53 +0100

    adds upload image component

---
+++ src/components/AccountComponents/UploadImage/UploadImage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const UploadImage = props => {
+    return (
+        <div>
+            <h3>Uploadimage works</h3>
+        </div>
+    );
+};
+
+
+export default UploadImage;
