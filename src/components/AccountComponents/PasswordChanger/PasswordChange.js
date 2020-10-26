6775bc4d034d40f3b8a6150cf3bd0f8176128c8b
Andy <aste.rix@icloud.com>
2020-10-26 20:03:21 +0100

    adds password change component

---
+++ src/components/AccountComponents/PasswordChanger/PasswordChange.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './PasswordChange.css';
+
+
+const PasswordChange = props => {
+    return (
+        <div>
+            <h3>Password change section works</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, laudantium!</p>
+        </div>
+    );
+};
+
+
+export default PasswordChange;
