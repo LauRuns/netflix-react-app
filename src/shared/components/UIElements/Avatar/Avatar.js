f18a2d678782d5a08e8f46dcc97f9fa38697a969
Andy <aste.rix@icloud.com>
2020-10-30 19:54:52 +0100

    adds log function to check if img has error for dev only

--- src/shared/components/UIElements/Avatar/Avatar.js
+++ src/shared/components/UIElements/Avatar/Avatar.js
@@ -3,9 +3,14 @@ import React from 'react';
 import './Avatar.css';
 
 const Avatar = (props) => {
+	const onImgErrorHandler = () => {
+		console.log('User Avatar has an error');
+	};
+
 	return (
 		<div className={`avatar ${props.className}`} style={props.style}>
 			<img
+				onError={onImgErrorHandler}
 				src={props.image ? props.image : './no-profile-picture.jpg'}
 				alt={props.alt}
 				style={{ width: props.width, height: props.height }}
