301594858da4714cd995a2b5117568940c6cd1ee
Andy <aste.rix@icloud.com>
2020-11-02 21:59:54 +0100

    removes component from countrylist

--- src/components/Countries/CountryList.js
+++ src/components/Countries/CountryList.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import Country from './Country/Country';
 import Card from '../../shared/components/UIElements/Card/Card';
 import Chip from '../../shared/components/UIElements/Chip/Chip';
 import './CountryList.css';
@@ -20,7 +19,6 @@ const CountryList = (props) => {
 		<React.Fragment>
 			<ul className="CountryList">
 				{props.items.map((country) => {
-					// return <Country key={country.countryId} country={country.country} />;
 					return <Chip key={country.countryId} name={country.country} />;
 				})}
 			</ul>
