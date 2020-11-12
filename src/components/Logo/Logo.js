b735c6bbedc2d522bc07c84711b4375f9ffbeced
Andy <aste.rix@icloud.com>
2020-11-12 19:36:49 +0100

    styling changes and adds variables with calc styling

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -7,10 +7,7 @@ import NetflixLogo from '../../assets/netflix_hexagon.png';
 const logo = (props) => {
 	return (
 		<div className="Logo">
-			<NavLink
-				to="/"
-				exact
-			>
+			<NavLink to="/" exact>
 				<img src={NetflixLogo} alt="Netflix-logo" />
 			</NavLink>
 		</div>
