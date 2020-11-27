4dc734a40c13aab7a08546cb940ba00c4aac1f57
Andy <aste.rix@icloud.com>
2020-11-27 21:42:59 +0100

    implements nav itemlist

--- src/Pages/MainNavigation/MainNavigation.js
+++ src/Pages/MainNavigation/MainNavigation.js
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 
 import Navbar from '../../shared/components/Navigation/Navbar/Navbar';
 import SideDrawer from '../../shared/components/Navigation/SideDrawer/SideDrawer';
 import Backdrop from '../../shared/components/UIElements/Backdrop/Backdrop';
+import { AuthContext } from '../../shared/context/auth-context';
+
 import './MainNavigation.scss';
 
 const MainNavigation = (props) => {
+	const auth = useContext(AuthContext);
 	const [drawerIsOpen, setDrawerIsOpen] = useState(false);
 
 	const openDrawerHandler = () => {
@@ -16,10 +19,38 @@ const MainNavigation = (props) => {
 		setDrawerIsOpen(!drawerIsOpen);
 	};
 
+	const logOut = () => {
+		auth.logout();
+	};
+
+	const navItemsList = [
+		{
+			linkTo: '/home',
+			linkName: 'Home'
+		},
+		{
+			linkTo: '/countries',
+			linkName: 'Countries'
+		},
+		{
+			linkTo: '/search',
+			linkName: 'Search'
+		},
+		{
+			linkTo: '/account',
+			linkName: 'Account'
+		}
+	];
+
 	return (
 		<React.Fragment>
 			{drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
-			<Navbar drawerToggleClicked={openDrawerHandler} />
+			<Navbar
+				navItemsList={navItemsList}
+				drawerToggleClicked={openDrawerHandler}
+				onLogout={logOut}
+				drawerIsOpen={drawerIsOpen}
+			/>
 			<SideDrawer open={drawerIsOpen} navItemClicked={closeDrawerHandler} />
 		</React.Fragment>
 	);
