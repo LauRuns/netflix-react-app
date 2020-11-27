3102b6bc1c0c68711f247d73b04a13ac0ce9b4ed
Andy <aste.rix@icloud.com>
2020-11-27 23:15:17 +0100

    adds tooltip to icon

--- src/shared/components/Navigation/Navbar/Navbar.js
+++ src/shared/components/Navigation/Navbar/Navbar.js
@@ -4,20 +4,27 @@ import NavItems from '../NavItems/NavItems';
 import Logo from '../../../../components/Logo/Logo';
 import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 import { Icon } from '../../../../components/Icon/Icon';
+import { Tooltip } from '../../UIElements/Tooltip/Tooltip';
 import './Navbar.scss';
 
-const Navbar = (props) => {
+const Navbar = ({ navItemsList, onLogout, drawerToggleClicked, isLoggedIn }) => {
 	return (
 		<header className="Navbar">
 			<div className="NavbarLogo">
 				<Logo />
 			</div>
 			<nav className="DesktopOnly">
-				<NavItems navItemsList={props.navItemsList} />
-				<Icon icon="exitApp" size={32} color="#fff" onClick={props.onLogout} />
+				<NavItems navItemsList={navItemsList} />
+				{isLoggedIn && (
+					<Tooltip direction="bottom" delay={200} message="Logout">
+						<div id="logout-icon-navbar">
+							<Icon icon="exitApp" size={32} color="#fff" onClick={onLogout} />
+						</div>
+					</Tooltip>
+				)}
 			</nav>
 			<menu className="MobileOnly">
-				<DrawerToggle clicked={props.drawerToggleClicked} />
+				<DrawerToggle clicked={drawerToggleClicked} />
 			</menu>
 		</header>
 	);
