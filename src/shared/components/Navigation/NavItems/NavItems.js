d0214d035a262280971c19f3566ebd02795e6da0
Andy <aste.rix@icloud.com>
2020-11-27 23:15:52 +0100

    removes unnecessary check

--- src/shared/components/Navigation/NavItems/NavItems.js
+++ src/shared/components/Navigation/NavItems/NavItems.js
@@ -1,13 +1,9 @@
-// /* eslint-disable jsx-a11y/interactive-supports-focus */
-// /* eslint-disable jsx-a11y/no-static-element-interactions */
 import React, { useContext } from 'react';
 
 import { AuthContext } from '../../../context/auth-context';
 import NavItem from './NavItem/NavItem';
-import { Tooltip } from '../../UIElements/Tooltip/Tooltip';
 
 import './NavItems.scss';
-import LogoutLogo from '../../../../assets/log-out.svg';
 
 const NavItems = ({ drawerIsOpen, navItemsList }) => {
 	const auth = useContext(AuthContext);
@@ -24,22 +20,6 @@ const NavItems = ({ drawerIsOpen, navItemsList }) => {
 						</NavItem>
 					);
 				})}
-
-			{auth.isLoggedIn &&
-				(drawerIsOpen ? (
-					<div onClick={auth.logout}>
-						<hr />
-						<strong>
-							<p>Logout</p>
-						</strong>
-					</div>
-				) : (
-					<div id="logout-icon-btn" className="logout-icon" onClick={auth.logout}>
-						<Tooltip direction="bottom" message="Logout, bye!" delay={100}>
-							<img src={LogoutLogo} alt="Logout icon" />
-						</Tooltip>
-					</div>
-				))}
 		</ul>
 	);
 };
