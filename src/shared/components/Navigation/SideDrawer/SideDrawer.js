5fbf9733096a15d1053b4c491840fac9e1017044
Andy <aste.rix@icloud.com>
2020-11-27 23:16:26 +0100

    destructures props and adds logout button

--- src/shared/components/Navigation/SideDrawer/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Logo from '../../../../components/Logo/Logo';
 import NavItems from '../NavItems/NavItems';
+import { AuthContext } from '../../../context/auth-context';
+import Button from '../../UIElements/Button/Button';
 
 import './SideDrawer.scss';
 
-const sideDrawer = (props) => {
+export const SideDrawer = ({ open, navItemClicked, navItems, isLoggedIn, onLogout }) => {
+	const auth = useContext(AuthContext);
+
 	let attachClasses = ['SideDrawer', 'Open', 'Close'];
 
-	if (props.open) {
+	if (open) {
 		attachClasses = ['SideDrawer', 'Open'];
 	}
 
@@ -18,12 +22,11 @@ const sideDrawer = (props) => {
 				<div className="Sidedrawerlogo">
 					<Logo />
 				</div>
-				<nav onClick={props.navItemClicked}>
-					<NavItems drawerIsOpen={props.open} />
+				<nav onClick={navItemClicked}>
+					<NavItems drawerIsOpen={open} navItemsList={navItems} />
+					{isLoggedIn && <Button onClick={onLogout}>LOGOUT</Button>}
 				</nav>
 			</div>
 		</React.Fragment>
 	);
 };
-
-export default sideDrawer;
