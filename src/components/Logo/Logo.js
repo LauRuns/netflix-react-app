f50f23dc7428b35a8e7c5981d99f8805a0f95a7e
Andy <aste.rix@icloud.com>
2020-11-28 13:35:18 +0100

    adds fix for logo component

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
 import './Logo.scss';
-import NetflixLogo from '../../assets/netflix_hexagon.png';
 
-const logo = (props) => {
+const defaultStyles = { display: 'inline-block', verticalAlign: 'middle' };
+
+export const Logo = ({ logoSource, altLogo, size, style, onClick }) => {
+	const defaultLogo = {
+		size: 16,
+		viewBox: '0 0 24 24',
+		style: {},
+		className: ''
+	};
+	const styles = { ...defaultStyles, ...style };
 	return (
-		<div className="Logo">
-			<NavLink to="/" exact>
-				<img src={NetflixLogo} alt="Netflix-logo" />
-			</NavLink>
+		<div className="logo-component-container">
+			<img
+				src={logoSource}
+				alt={altLogo}
+				style={styles}
+				width={`${size || defaultLogo.size}px`}
+				height={`${size || defaultLogo.size}px`}
+				onClick={onClick}
+			/>
 		</div>
 	);
 };
-
-export default logo;
