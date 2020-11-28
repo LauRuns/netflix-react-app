c659461a5b184509c7674e23e310c938331d4ed2
Andy <aste.rix@icloud.com>
2020-11-28 12:20:41 +0100

    adds component for rendenring list of nav icons

---
+++ src/shared/components/Navigation/iconNavItems/IconNavItems.js
@@ -0,0 +1,30 @@
+import React, { useContext } from 'react';
+
+import { AuthContext } from '../../../context/auth-context';
+import { IconNavItem } from '../iconNavItem/IconNavItem';
+
+import './IconNavItems.scss';
+
+export const IconNavItems = ({ navItems }) => {
+	const auth = useContext(AuthContext);
+	return (
+		<ul className="icon-nav-items-container">
+			{auth.isLoggedIn &&
+				navItems &&
+				navItems.map(({ linkTo, linkName, iconName, iconSize, iconColor }) => {
+					console.log(linkName, iconName);
+					return (
+						<IconNavItem
+							key={linkName}
+							link={linkTo}
+							iconName={iconName}
+							iconSize={iconSize}
+							iconColor={iconColor}
+						>
+							{linkName}
+						</IconNavItem>
+					);
+				})}
+		</ul>
+	);
+};
