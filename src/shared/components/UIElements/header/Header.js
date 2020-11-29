e8896c197b30f64fb6f693b1709a6f7a00c2d422
Andy <aste.rix@icloud.com>
2020-11-29 20:53:07 +0100

    adds header styling

--- src/shared/components/UIElements/header/Header.js
+++ src/shared/components/UIElements/header/Header.js
@@ -2,13 +2,18 @@ import React from 'react';
 
 import './Header.scss';
 
-export const Header = ({ children, center, md, lg, xl }) => {
+const defaultHeader = { display: 'flex' };
+
+export const Header = ({ children, center, md, lg, xl, style, title }) => {
+	const styles = { ...defaultHeader, ...style };
 	return (
 		<div
 			className={`header-item ${center && 'header-center'} ${md && 'header-medium'} ${
 				lg && 'header-large'
 			} ${xl && 'header-x-large'}`}
+			style={styles}
 		>
+			{title && <h2>{title}</h2>}
 			{children}
 		</div>
 	);
