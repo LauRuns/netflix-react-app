39310b8e993f092f83f2f189695d55995862c953
Andy <aste.rix@icloud.com>
2020-11-29 21:31:09 +0100

    adds default styling

--- src/shared/components/Navigation/iconNavItem/IconNavItem.js
+++ src/shared/components/Navigation/iconNavItem/IconNavItem.js
@@ -5,13 +5,27 @@ import { Icon } from '../../../../components/Icon/Icon';
 
 import './IconNavItem.scss';
 
-export const IconNavItem = ({ children, link, iconName, iconSize, iconColor, iconStyle }) => {
+export const IconNavItem = ({
+	children,
+	link,
+	iconName,
+	iconSize,
+	iconColor,
+	iconStyle,
+	textSize
+}) => {
+	const defaultNavItemStyle = {
+		color: '#fff',
+		fontSize: 'medium'
+	};
+	const styles = { ...defaultNavItemStyle, ...textSize };
+
 	return (
-		<li className="icon-nav-item">
+		<div className="icon-nav-item" style={styles}>
 			<NavLink to={link} exact activeClassName="active">
 				<Icon icon={iconName} size={iconSize} color={iconColor} style={iconStyle} />
 				{children}
 			</NavLink>
-		</li>
+		</div>
 	);
 };
