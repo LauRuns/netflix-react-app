60d09ca828dac4dbcdeb6f0f75cdfa9ed77aebcb
Andy <aste.rix@icloud.com>
2020-11-29 20:56:42 +0100

    updates import statement

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ import {
 
 import MainNavigation from './Pages/MainNavigation/MainNavigation';
 import Auth from './Pages/AuthPage/Auth';
-import CountriesPageContainer from './Pages/CountriesPage/CountriesPageContainer';
-import CountryDetailPage from './Pages/CountriesPage/CountryDetailPage/CountryDetailPage';
+import { CountriesPage } from './Pages/CountriesPage/CountriesPage';
+import { CountryDetailPage } from './components/countryDetailPage/CountryDetailPage';
+
 import SearchPage from './Pages/SearchPage/SearchPage';
 import { UserAccount } from './Pages/Account/Account';
 import LandingPage from './Pages/LandingPage/LandingPage';
@@ -28,7 +29,7 @@ const App = () => {
 			<Switch>
 				<Route path="/" exact component={Auth} />
 				<Route path="/auth" exact component={Auth} />
-				<Route path="/countries" exact component={CountriesPageContainer} />
+				<Route path="/countries" exact component={CountriesPage} />
 				<Route path="/account" exact component={UserAccount} />
 				<Route path="/countryinfo" exact component={CountryDetailPage} />
 				<Route path="/search" exact component={SearchPage} />
