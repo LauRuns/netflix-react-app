7de25f2913e4fe8acd279d49f838ae365f8e5d15
Andy <aste.rix@icloud.com>
2020-11-29 14:05:37 +0100

    adds new testitems

--- src/assets/testitems.js
+++ src/assets/testitems.js
@@ -35,213 +35,152 @@ export const testitems = [
 		year: 2011
 	},
 	{
-		avgrating: 4.2888327,
-		curdate: '2020-11-28',
-		imdbawards: '1 win & 3 nominations.',
-		imdbcountry: 'Japan',
-		imdbgenre: 'Animation, Action, Drama, Horror, Thriller',
-		imdbid: 'tt1909447',
-		imdblanguage: 'Japanese, English',
-		imdblocalimage: 1,
-		imdbmetascore: 'N/A',
-		imdbplot:
-			'Ganta, the sole survivor of the brutal murder of his entire class, is framed and ends up falsely convicted for it. During the massacre the real killer embeds a red crystal in him. Ganta is sent to a privately owned prison which has a rather different and perverted take on incarceration, entertaining the masses by performing dangerous games in exchange for privileges. Every inmate he encounters has their own goals, survival being the main one, and it is now up to him to survive this and maybe even prove his innocence.',
-		imdbposter:
-			'https://images-na.ssl-images-amazon.com/images/M/MV5BYTUxNjUyNDQtYzk3ZS00NWY0LTllMDctYjQyNzBmNTIwY2M2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg',
-		imdbrated: 'TV-MA',
-		imdbrating: 7.2,
-		imdbruntime: '24 min',
-		imdbvotes: 8133,
+		avgrating: 3.7538462,
+		clist:
+			"CA:'Canada',FR:France,DE:Germany,NL:Netherlands,PL:Poland,GB:United Kingdom,US:United States,AR:Argentina,AU:Australia,BE:Belgium",
+
+		id: 51295,
+		imdbid: 'tt5339440',
+		imdbrating: 8.2,
 		img:
-			'https://occ-0-768-769.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABX8_JnjMS-G47JscmOxlavFbj5QYzV5oe_N6icZpxLfNjW0LMwcWkOxDoudko7bwYBbwJ15eSmA2wWW9tOl_ccdIzQ.jpg?r=6c5',
-		lgimg:
-			'https://occ-0-55-56.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABbypzBw91rwf3w_6ICXOv6oquOtUOQle74Gtf3M4lUFbe_zmFx75MjZXg0Ic8A3xWu8Dp1bU6DEcYGr0NwebJbEYC0Zq.jpg?r=a9f',
-		matlabel: 'Suitable for age 16 or older.',
-		matlevel: '100',
-		netflixid: 80005822,
-		netflixruntime: 0,
-		nfdate: '2015-04-14',
-		nfid: 80005822,
-		predrating: 4.8,
+			'https://occ-0-2851-38.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABWCMFs33VXs38KetgHQyrMXRHTdQngeCqNykNGWPczbjorYVhQKQs6giTijxsEsQNlekzBA6BCoQ5Z7UYEgRcKldb5x5M-06SwlqythHOdZY5Ie8qpDyyCGKO1M.jpg?r=b3c',
+		nfid: 80095532,
+		poster:
+			'https://images-na.ssl-images-amazon.com/images/M/MV5BOTEzMjc3MDI3M15BMl5BanBnXkFtZTgwMDU0MDA4MDI@._V1_SX300.jpg',
+		runtime: 0,
 		synopsis:
-			'Falsely convicted for the mass murder of his classmates, a schoolboy must endure a nightmarish penal system, all while trying to prove his innocence.',
-		title: 'Deadman Wonderland',
+			'In a reimagining of the TV classic, a newly single Latina mother raises her teen daughter and tween son with the &#39;help&#39; of her old-school mom.',
+		title: 'One Day at a Time',
+		titledate: '2017-01-06',
+		top250: 0,
+		top250tv: 0,
 		vtype: 'series',
-		year: 2011
+		year: 2017
 	},
 	{
-		avgrating: 4.2888327,
-		curdate: '2020-11-28',
-		imdbawards: '1 win & 3 nominations.',
-		imdbcountry: 'Japan',
-		imdbgenre: 'Animation, Action, Drama, Horror, Thriller',
-		imdbid: 'tt1909447',
-		imdblanguage: 'Japanese, English',
-		imdblocalimage: 1,
-		imdbmetascore: 'N/A',
-		imdbplot:
-			'Ganta, the sole survivor of the brutal murder of his entire class, is framed and ends up falsely convicted for it. During the massacre the real killer embeds a red crystal in him. Ganta is sent to a privately owned prison which has a rather different and perverted take on incarceration, entertaining the masses by performing dangerous games in exchange for privileges. Every inmate he encounters has their own goals, survival being the main one, and it is now up to him to survive this and maybe even prove his innocence.',
-		imdbposter:
-			'https://images-na.ssl-images-amazon.com/images/M/MV5BYTUxNjUyNDQtYzk3ZS00NWY0LTllMDctYjQyNzBmNTIwY2M2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg',
-		imdbrated: 'TV-MA',
-		imdbrating: 7.2,
-		imdbruntime: '24 min',
-		imdbvotes: 8133,
+		avgrating: 4.3109455,
+		clist:
+			"CA:'Canada',FR:France,DE:Germany,NL:Netherlands,PL:Poland,GB:United Kingdom,US:United States,AR:Argentina,AU:Australia,BE:Belgium",
+
+		id: 51296,
+		imdbid: 'tt6216718',
+		imdbrating: 4.8,
 		img:
-			'https://occ-0-768-769.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABX8_JnjMS-G47JscmOxlavFbj5QYzV5oe_N6icZpxLfNjW0LMwcWkOxDoudko7bwYBbwJ15eSmA2wWW9tOl_ccdIzQ.jpg?r=6c5',
-		lgimg:
-			'https://occ-0-55-56.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABbypzBw91rwf3w_6ICXOv6oquOtUOQle74Gtf3M4lUFbe_zmFx75MjZXg0Ic8A3xWu8Dp1bU6DEcYGr0NwebJbEYC0Zq.jpg?r=a9f',
-		matlabel: 'Suitable for age 16 or older.',
-		matlevel: '100',
-		netflixid: 80005822,
-		netflixruntime: 0,
-		nfdate: '2015-04-14',
-		nfid: 80005822,
-		predrating: 4.8,
+			'https://occ-0-2851-38.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABZLDLRgKAvgR1r6QqG1Tz3iQAmB-P0uq-58xvbqa97wbEx2xfxjDqEKB7vJUp0Tb8zfe1RWgqlzhHJxrOjvX3SFuXBuIqTv2Snen61SzTW3qlYnUN535K1Co05g.jpg?r=c10',
+		nfid: 80057506,
+		poster:
+			'https://images-na.ssl-images-amazon.com/images/M/MV5BNjFkY2RmZWItNDc3OS00MjM4LWJiNzYtYzM0ODEyMDZjZGVjL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNzEwOTk4NzQ@._V1_SX300.jpg',
+		runtime: 0,
 		synopsis:
-			'Falsely convicted for the mass murder of his classmates, a schoolboy must endure a nightmarish penal system, all while trying to prove his innocence.',
-		title: 'Deadman Wonderland',
+			'Saved from a plane crash and given supernatural powers, teen Tarzan joins forces with brave city girl Jane to protect his jungle home from threats.',
+		title: 'Edgar Rice Burroughs&#39; Tarzan and Jane',
+		titledate: '2017-01-06',
+		top250: 0,
+		top250tv: 0,
 		vtype: 'series',
-		year: 2011
+		year: 2017
 	},
 	{
-		avgrating: 4.2888327,
-		curdate: '2020-11-28',
-		imdbawards: '1 win & 3 nominations.',
-		imdbcountry: 'Japan',
-		imdbgenre: 'Animation, Action, Drama, Horror, Thriller',
-		imdbid: 'tt1909447',
-		imdblanguage: 'Japanese, English',
-		imdblocalimage: 1,
-		imdbmetascore: 'N/A',
-		imdbplot:
-			'Ganta, the sole survivor of the brutal murder of his entire class, is framed and ends up falsely convicted for it. During the massacre the real killer embeds a red crystal in him. Ganta is sent to a privately owned prison which has a rather different and perverted take on incarceration, entertaining the masses by performing dangerous games in exchange for privileges. Every inmate he encounters has their own goals, survival being the main one, and it is now up to him to survive this and maybe even prove his innocence.',
-		imdbposter:
-			'https://images-na.ssl-images-amazon.com/images/M/MV5BYTUxNjUyNDQtYzk3ZS00NWY0LTllMDctYjQyNzBmNTIwY2M2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg',
-		imdbrated: 'TV-MA',
-		imdbrating: 7.2,
-		imdbruntime: '24 min',
-		imdbvotes: 8133,
+		avgrating: 3.56,
+		clist:
+			"CA:'Canada',FR:France,DE:Germany,NL:Netherlands,PL:Poland,GB:United Kingdom,US:United States,AR:Argentina,AU:Australia,BE:Belgium",
+
+		id: 51297,
+		imdbid: 'tt5740806',
+		imdbrating: 4.8,
 		img:
-			'https://occ-0-768-769.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABX8_JnjMS-G47JscmOxlavFbj5QYzV5oe_N6icZpxLfNjW0LMwcWkOxDoudko7bwYBbwJ15eSmA2wWW9tOl_ccdIzQ.jpg?r=6c5',
-		lgimg:
-			'https://occ-0-55-56.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABbypzBw91rwf3w_6ICXOv6oquOtUOQle74Gtf3M4lUFbe_zmFx75MjZXg0Ic8A3xWu8Dp1bU6DEcYGr0NwebJbEYC0Zq.jpg?r=a9f',
-		matlabel: 'Suitable for age 16 or older.',
-		matlevel: '100',
-		netflixid: 80005822,
-		netflixruntime: 0,
-		nfdate: '2015-04-14',
-		nfid: 80005822,
-		predrating: 4.8,
+			'https://occ-0-2851-38.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABeiYpUFpYFGfsbf2N4ikR4ixRkaWyXuedQdObfzOALdr9J97RKKL4zuUO94ERiGB1DwNGs4mtDsp2dwee6EeEnHYi_0rPAEYjLaoSgMVFblZLth2Y9fMU_OEZ1c.jpg?r=077',
+		nfid: 80097725,
+		poster:
+			'https://images-na.ssl-images-amazon.com/images/M/MV5BNDM4NzhhNTQtZmUyMy00OTFlLWFlMzgtYTQ5OTE3YjBmMTA5L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjczOTE0MzM@._V1_SX300.jpg',
+		runtime: 5864,
 		synopsis:
-			'Falsely convicted for the mass murder of his classmates, a schoolboy must endure a nightmarish penal system, all while trying to prove his innocence.',
-		title: 'Deadman Wonderland',
-		vtype: 'series',
-		year: 2011
+			'When a crisis threatens to destroy their high school, four teens hatch a daring plan to raise $10 million. Step one? Breaking into the U.S. Mint.',
+		title: 'Coin Heist',
+		titledate: '2017-01-06',
+		top250: 0,
+		top250tv: 0,
+		vtype: 'movie',
+		year: 2017
 	},
 	{
-		avgrating: 4.2888327,
-		curdate: '2020-11-28',
-		imdbawards: '1 win & 3 nominations.',
-		imdbcountry: 'Japan',
-		imdbgenre: 'Animation, Action, Drama, Horror, Thriller',
-		imdbid: 'tt1909447',
-		imdblanguage: 'Japanese, English',
-		imdblocalimage: 1,
-		imdbmetascore: 'N/A',
-		imdbplot:
-			'Ganta, the sole survivor of the brutal murder of his entire class, is framed and ends up falsely convicted for it. During the massacre the real killer embeds a red crystal in him. Ganta is sent to a privately owned prison which has a rather different and perverted take on incarceration, entertaining the masses by performing dangerous games in exchange for privileges. Every inmate he encounters has their own goals, survival being the main one, and it is now up to him to survive this and maybe even prove his innocence.',
-		imdbposter:
-			'https://images-na.ssl-images-amazon.com/images/M/MV5BYTUxNjUyNDQtYzk3ZS00NWY0LTllMDctYjQyNzBmNTIwY2M2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg',
-		imdbrated: 'TV-MA',
-		imdbrating: 7.2,
-		imdbruntime: '24 min',
-		imdbvotes: 8133,
+		avgrating: 3.064516,
+		clist:
+			"CA:'Canada',FR:France,DE:Germany,NL:Netherlands,PL:Poland,GB:United Kingdom,US:United States,AR:Argentina,AU:Australia,BE:Belgium",
+
+		id: 50763,
+		imdbid: 'tt4040530',
+		imdbrating: 7.4,
 		img:
-			'https://occ-0-768-769.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABX8_JnjMS-G47JscmOxlavFbj5QYzV5oe_N6icZpxLfNjW0LMwcWkOxDoudko7bwYBbwJ15eSmA2wWW9tOl_ccdIzQ.jpg?r=6c5',
-		lgimg:
-			'https://occ-0-55-56.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABbypzBw91rwf3w_6ICXOv6oquOtUOQle74Gtf3M4lUFbe_zmFx75MjZXg0Ic8A3xWu8Dp1bU6DEcYGr0NwebJbEYC0Zq.jpg?r=a9f',
-		matlabel: 'Suitable for age 16 or older.',
-		matlevel: '100',
-		netflixid: 80005822,
-		netflixruntime: 0,
-		nfdate: '2015-04-14',
-		nfid: 80005822,
-		predrating: 4.8,
+			'https://occ-0-2851-38.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABfcpR6CWhPg0KJZTiwfXmzKY36mLTTm7ZwGflaTzLRH5K3aHzkmH36JPmzY3bXZTnRAni4HDYWDhe713QBNzBmUeOg.jpg?r=a11',
+		nfid: 80091880,
+		poster:
+			'https://images-na.ssl-images-amazon.com/images/M/MV5BYWZiMTAxNWYtYzRjMC00ZWUxLTkzMWQtOWRlM2U4NGFkZGJlL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNDIwMjMwMjg@._V1_SX300.jpg',
+		runtime: 0,
 		synopsis:
-			'Falsely convicted for the mass murder of his classmates, a schoolboy must endure a nightmarish penal system, all while trying to prove his innocence.',
-		title: 'Deadman Wonderland',
+			'In the wake of World War II, 11 Allied judges are tasked with weighing the fates of Japanese war criminals in a tense international trial.',
+		title: 'Tokyo Trial',
+		titledate: '2016-12-13',
+		top250: 0,
+		top250tv: 0,
 		vtype: 'series',
-		year: 2011
+		year: 2017
 	},
 	{
-		avgrating: 4.2888327,
+		avgrating: 0,
 		curdate: '2020-11-28',
-		imdbawards: '1 win & 3 nominations.',
-		imdbcountry: 'Japan',
-		imdbgenre: 'Animation, Action, Drama, Horror, Thriller',
-		imdbid: 'tt1909447',
-		imdblanguage: 'Japanese, English',
-		imdblocalimage: 1,
+		imdbawards: '8 wins & 3 nominations.',
+		imdbcountry: 'Israel',
+		imdbgenre: 'Drama',
+		imdbid: 'tt3069894',
+		imdblanguage: 'Hebrew, Yiddish',
+		imdblocalimage: null,
 		imdbmetascore: 'N/A',
 		imdbplot:
-			'Ganta, the sole survivor of the brutal murder of his entire class, is framed and ends up falsely convicted for it. During the massacre the real killer embeds a red crystal in him. Ganta is sent to a privately owned prison which has a rather different and perverted take on incarceration, entertaining the masses by performing dangerous games in exchange for privileges. Every inmate he encounters has their own goals, survival being the main one, and it is now up to him to survive this and maybe even prove his innocence.',
+			'The story of a haredi family in Jerusalem. Shulem Schtisel the father of a large family is dealing with his younger son who he wants to see married soon. His daughter, Giti is dealing in secret with marital problems while his other son Zvi Arie tries to patch everything up.',
 		imdbposter:
-			'https://images-na.ssl-images-amazon.com/images/M/MV5BYTUxNjUyNDQtYzk3ZS00NWY0LTllMDctYjQyNzBmNTIwY2M2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg',
-		imdbrated: 'TV-MA',
-		imdbrating: 7.2,
-		imdbruntime: '24 min',
-		imdbvotes: 8133,
+			'https://images-na.ssl-images-amazon.com/images/M/MV5BYjQ3MGNkN2MtZjUxZC00NjdmLWE4ZmItMGU4YTkwOWYxOGRhXkEyXkFqcGdeQXVyMjMyMzI4MzY@._V1_SX300.jpg',
+		imdbrated: 'N/A',
+		imdbrating: 8.7,
+		imdbruntime: '45 min',
+		imdbvotes: 1757,
 		img:
-			'https://occ-0-768-769.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABX8_JnjMS-G47JscmOxlavFbj5QYzV5oe_N6icZpxLfNjW0LMwcWkOxDoudko7bwYBbwJ15eSmA2wWW9tOl_ccdIzQ.jpg?r=6c5',
+			'https://occ-0-2851-38.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABclCe_DHXX3XpJaURd8H0wx6rLHO23u1rWynvynzz6VIu4fFgR0erMW9GKsAmzV8v_9EONDfxaq-FzGkppDc-M-UWA.jpg?r=a2b',
 		lgimg:
-			'https://occ-0-55-56.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABbypzBw91rwf3w_6ICXOv6oquOtUOQle74Gtf3M4lUFbe_zmFx75MjZXg0Ic8A3xWu8Dp1bU6DEcYGr0NwebJbEYC0Zq.jpg?r=a9f',
-		matlabel: 'Suitable for age 16 or older.',
-		matlevel: '100',
-		netflixid: 80005822,
+			'https://occ-0-784-778.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABYoUIFy_xRSJvWBNxxa7ySZxxwCQkj702j2hFbvc3H3yyHEzi0THnkDsZzhDAm3jf7BLdGcUi1llqvrFm0wiN5MeXGSi.jpg?r=a2b',
+		matlabel: 'All Ages',
+		matlevel: null,
+		netflixid: 81004164,
 		netflixruntime: 0,
-		nfdate: '2015-04-14',
-		nfid: 80005822,
-		predrating: 4.8,
+		nfdate: '2018-12-15',
+		nfid: 81004164,
+		predrating: null,
 		synopsis:
-			'Falsely convicted for the mass murder of his classmates, a schoolboy must endure a nightmarish penal system, all while trying to prove his innocence.',
-		title: 'Deadman Wonderland',
+			'A Haredi family living in an ultra-Orthodox neighborhood of Jerusalem reckons with love, loss and the doldrums of daily life.',
+		title: 'Shtisel',
 		vtype: 'series',
-		year: 2011
+		year: 2015
 	},
 	{
-		avgrating: 4.2888327,
-		curdate: '2020-11-28',
-		imdbawards: '1 win & 3 nominations.',
-		imdbcountry: 'Japan',
-		imdbgenre: 'Animation, Action, Drama, Horror, Thriller',
-		imdbid: 'tt1909447',
-		imdblanguage: 'Japanese, English',
-		imdblocalimage: 1,
-		imdbmetascore: 'N/A',
-		imdbplot:
-			'Ganta, the sole survivor of the brutal murder of his entire class, is framed and ends up falsely convicted for it. During the massacre the real killer embeds a red crystal in him. Ganta is sent to a privately owned prison which has a rather different and perverted take on incarceration, entertaining the masses by performing dangerous games in exchange for privileges. Every inmate he encounters has their own goals, survival being the main one, and it is now up to him to survive this and maybe even prove his innocence.',
-		imdbposter:
-			'https://images-na.ssl-images-amazon.com/images/M/MV5BYTUxNjUyNDQtYzk3ZS00NWY0LTllMDctYjQyNzBmNTIwY2M2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg',
-		imdbrated: 'TV-MA',
-		imdbrating: 7.2,
-		imdbruntime: '24 min',
-		imdbvotes: 8133,
+		avgrating: 4.0333333,
+		clist:
+			"CA:'Canada',FR:France,DE:Germany,NL:Netherlands,PL:Poland,GB:United Kingdom,US:United States,AR:Argentina,AU:Australia,BE:Belgium",
+		id: 51594,
+		imdbid: 'tt5420376',
+		imdbrating: 7.1,
 		img:
-			'https://occ-0-768-769.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABX8_JnjMS-G47JscmOxlavFbj5QYzV5oe_N6icZpxLfNjW0LMwcWkOxDoudko7bwYBbwJ15eSmA2wWW9tOl_ccdIzQ.jpg?r=6c5',
-		lgimg:
-			'https://occ-0-55-56.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABbypzBw91rwf3w_6ICXOv6oquOtUOQle74Gtf3M4lUFbe_zmFx75MjZXg0Ic8A3xWu8Dp1bU6DEcYGr0NwebJbEYC0Zq.jpg?r=a9f',
-		matlabel: 'Suitable for age 16 or older.',
-		matlevel: '100',
-		netflixid: 80005822,
-		netflixruntime: 0,
-		nfdate: '2015-04-14',
-		nfid: 80005822,
-		predrating: 4.8,
+			'https://occ-0-2851-38.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABcLMEeFr_W6oLoKFBPnS0V8o60KK1MjTgsTBd6MhAWEtSMPtmjzi3kRCk0PURxrliV-1EyC3gmA1BJkfeqzjQxYshXUs1hIO9xk-TKhG5Vh1Zb9KAvsKJkbn5tz_lYW-mdpfSPDAPZbt3vgqcxwMSxIO0cZg.jpg?r=624',
+		nfid: 80133311,
+		poster:
+			'https://images-na.ssl-images-amazon.com/images/M/MV5BNTkzOTA4ODE3Nl5BMl5BanBnXkFtZTgwODIzMTQzMTI@._V1_SX300.jpg',
+		runtime: 0,
 		synopsis:
-			'Falsely convicted for the mass murder of his classmates, a schoolboy must endure a nightmarish penal system, all while trying to prove his innocence.',
-		title: 'Deadman Wonderland',
+			'While navigating the troubled waters of sex, romance, school and family, teen Archie and his gang become entangled in a dark Riverdale mystery.',
+		title: 'Riverdale',
+		titledate: '2017-01-28',
+		top250: 0,
+		top250tv: 0,
 		vtype: 'series',
-		year: 2011
+		year: 2017
 	}
 ];
