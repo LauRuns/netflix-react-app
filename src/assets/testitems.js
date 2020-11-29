9b3fde03a0ae16b20109c043caef3187274dd274
Andy <aste.rix@icloud.com>
2020-11-29 11:42:48 +0100

    adds test items

---
+++ src/assets/testitems.js
@@ -0,0 +1,247 @@
+export const testitems = [
+	{
+		avgrating: 4.2888327,
+		curdate: '2020-11-28',
+		imdbawards: '1 win & 3 nominations.',
+		imdbcountry: 'Japan',
+		imdbgenre: 'Animation, Action, Drama, Horror, Thriller',
+		imdbid: 'tt1909447',
+		imdblanguage: 'Japanese, English',
+		imdblocalimage: 1,
+		imdbmetascore: 'N/A',
+		imdbplot:
+			'Ganta, the sole survivor of the brutal murder of his entire class, is framed and ends up falsely convicted for it. During the massacre the real killer embeds a red crystal in him. Ganta is sent to a privately owned prison which has a rather different and perverted take on incarceration, entertaining the masses by performing dangerous games in exchange for privileges. Every inmate he encounters has their own goals, survival being the main one, and it is now up to him to survive this and maybe even prove his innocence.',
+		imdbposter:
+			'https://images-na.ssl-images-amazon.com/images/M/MV5BYTUxNjUyNDQtYzk3ZS00NWY0LTllMDctYjQyNzBmNTIwY2M2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg',
+		imdbrated: 'TV-MA',
+		imdbrating: 7.2,
+		imdbruntime: '24 min',
+		imdbvotes: 8133,
+		img:
+			'https://occ-0-768-769.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABX8_JnjMS-G47JscmOxlavFbj5QYzV5oe_N6icZpxLfNjW0LMwcWkOxDoudko7bwYBbwJ15eSmA2wWW9tOl_ccdIzQ.jpg?r=6c5',
+		lgimg:
+			'https://occ-0-55-56.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABbypzBw91rwf3w_6ICXOv6oquOtUOQle74Gtf3M4lUFbe_zmFx75MjZXg0Ic8A3xWu8Dp1bU6DEcYGr0NwebJbEYC0Zq.jpg?r=a9f',
+		matlabel: 'Suitable for age 16 or older.',
+		matlevel: '100',
+		netflixid: 80005822,
+		netflixruntime: 0,
+		nfdate: '2015-04-14',
+		nfid: 80005822,
+		predrating: 4.8,
+		synopsis:
+			'Falsely convicted for the mass murder of his classmates, a schoolboy must endure a nightmarish penal system, all while trying to prove his innocence.',
+		title: 'Deadman Wonderland',
+		vtype: 'series',
+		year: 2011
+	},
+	{
+		avgrating: 4.2888327,
+		curdate: '2020-11-28',
+		imdbawards: '1 win & 3 nominations.',
+		imdbcountry: 'Japan',
+		imdbgenre: 'Animation, Action, Drama, Horror, Thriller',
+		imdbid: 'tt1909447',
+		imdblanguage: 'Japanese, English',
+		imdblocalimage: 1,
+		imdbmetascore: 'N/A',
+		imdbplot:
+			'Ganta, the sole survivor of the brutal murder of his entire class, is framed and ends up falsely convicted for it. During the massacre the real killer embeds a red crystal in him. Ganta is sent to a privately owned prison which has a rather different and perverted take on incarceration, entertaining the masses by performing dangerous games in exchange for privileges. Every inmate he encounters has their own goals, survival being the main one, and it is now up to him to survive this and maybe even prove his innocence.',
+		imdbposter:
+			'https://images-na.ssl-images-amazon.com/images/M/MV5BYTUxNjUyNDQtYzk3ZS00NWY0LTllMDctYjQyNzBmNTIwY2M2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg',
+		imdbrated: 'TV-MA',
+		imdbrating: 7.2,
+		imdbruntime: '24 min',
+		imdbvotes: 8133,
+		img:
+			'https://occ-0-768-769.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABX8_JnjMS-G47JscmOxlavFbj5QYzV5oe_N6icZpxLfNjW0LMwcWkOxDoudko7bwYBbwJ15eSmA2wWW9tOl_ccdIzQ.jpg?r=6c5',
+		lgimg:
+			'https://occ-0-55-56.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABbypzBw91rwf3w_6ICXOv6oquOtUOQle74Gtf3M4lUFbe_zmFx75MjZXg0Ic8A3xWu8Dp1bU6DEcYGr0NwebJbEYC0Zq.jpg?r=a9f',
+		matlabel: 'Suitable for age 16 or older.',
+		matlevel: '100',
+		netflixid: 80005822,
+		netflixruntime: 0,
+		nfdate: '2015-04-14',
+		nfid: 80005822,
+		predrating: 4.8,
+		synopsis:
+			'Falsely convicted for the mass murder of his classmates, a schoolboy must endure a nightmarish penal system, all while trying to prove his innocence.',
+		title: 'Deadman Wonderland',
+		vtype: 'series',
+		year: 2011
+	},
+	{
+		avgrating: 4.2888327,
+		curdate: '2020-11-28',
+		imdbawards: '1 win & 3 nominations.',
+		imdbcountry: 'Japan',
+		imdbgenre: 'Animation, Action, Drama, Horror, Thriller',
+		imdbid: 'tt1909447',
+		imdblanguage: 'Japanese, English',
+		imdblocalimage: 1,
+		imdbmetascore: 'N/A',
+		imdbplot:
+			'Ganta, the sole survivor of the brutal murder of his entire class, is framed and ends up falsely convicted for it. During the massacre the real killer embeds a red crystal in him. Ganta is sent to a privately owned prison which has a rather different and perverted take on incarceration, entertaining the masses by performing dangerous games in exchange for privileges. Every inmate he encounters has their own goals, survival being the main one, and it is now up to him to survive this and maybe even prove his innocence.',
+		imdbposter:
+			'https://images-na.ssl-images-amazon.com/images/M/MV5BYTUxNjUyNDQtYzk3ZS00NWY0LTllMDctYjQyNzBmNTIwY2M2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg',
+		imdbrated: 'TV-MA',
+		imdbrating: 7.2,
+		imdbruntime: '24 min',
+		imdbvotes: 8133,
+		img:
+			'https://occ-0-768-769.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABX8_JnjMS-G47JscmOxlavFbj5QYzV5oe_N6icZpxLfNjW0LMwcWkOxDoudko7bwYBbwJ15eSmA2wWW9tOl_ccdIzQ.jpg?r=6c5',
+		lgimg:
+			'https://occ-0-55-56.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABbypzBw91rwf3w_6ICXOv6oquOtUOQle74Gtf3M4lUFbe_zmFx75MjZXg0Ic8A3xWu8Dp1bU6DEcYGr0NwebJbEYC0Zq.jpg?r=a9f',
+		matlabel: 'Suitable for age 16 or older.',
+		matlevel: '100',
+		netflixid: 80005822,
+		netflixruntime: 0,
+		nfdate: '2015-04-14',
+		nfid: 80005822,
+		predrating: 4.8,
+		synopsis:
+			'Falsely convicted for the mass murder of his classmates, a schoolboy must endure a nightmarish penal system, all while trying to prove his innocence.',
+		title: 'Deadman Wonderland',
+		vtype: 'series',
+		year: 2011
+	},
+	{
+		avgrating: 4.2888327,
+		curdate: '2020-11-28',
+		imdbawards: '1 win & 3 nominations.',
+		imdbcountry: 'Japan',
+		imdbgenre: 'Animation, Action, Drama, Horror, Thriller',
+		imdbid: 'tt1909447',
+		imdblanguage: 'Japanese, English',
+		imdblocalimage: 1,
+		imdbmetascore: 'N/A',
+		imdbplot:
+			'Ganta, the sole survivor of the brutal murder of his entire class, is framed and ends up falsely convicted for it. During the massacre the real killer embeds a red crystal in him. Ganta is sent to a privately owned prison which has a rather different and perverted take on incarceration, entertaining the masses by performing dangerous games in exchange for privileges. Every inmate he encounters has their own goals, survival being the main one, and it is now up to him to survive this and maybe even prove his innocence.',
+		imdbposter:
+			'https://images-na.ssl-images-amazon.com/images/M/MV5BYTUxNjUyNDQtYzk3ZS00NWY0LTllMDctYjQyNzBmNTIwY2M2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg',
+		imdbrated: 'TV-MA',
+		imdbrating: 7.2,
+		imdbruntime: '24 min',
+		imdbvotes: 8133,
+		img:
+			'https://occ-0-768-769.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABX8_JnjMS-G47JscmOxlavFbj5QYzV5oe_N6icZpxLfNjW0LMwcWkOxDoudko7bwYBbwJ15eSmA2wWW9tOl_ccdIzQ.jpg?r=6c5',
+		lgimg:
+			'https://occ-0-55-56.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABbypzBw91rwf3w_6ICXOv6oquOtUOQle74Gtf3M4lUFbe_zmFx75MjZXg0Ic8A3xWu8Dp1bU6DEcYGr0NwebJbEYC0Zq.jpg?r=a9f',
+		matlabel: 'Suitable for age 16 or older.',
+		matlevel: '100',
+		netflixid: 80005822,
+		netflixruntime: 0,
+		nfdate: '2015-04-14',
+		nfid: 80005822,
+		predrating: 4.8,
+		synopsis:
+			'Falsely convicted for the mass murder of his classmates, a schoolboy must endure a nightmarish penal system, all while trying to prove his innocence.',
+		title: 'Deadman Wonderland',
+		vtype: 'series',
+		year: 2011
+	},
+	{
+		avgrating: 4.2888327,
+		curdate: '2020-11-28',
+		imdbawards: '1 win & 3 nominations.',
+		imdbcountry: 'Japan',
+		imdbgenre: 'Animation, Action, Drama, Horror, Thriller',
+		imdbid: 'tt1909447',
+		imdblanguage: 'Japanese, English',
+		imdblocalimage: 1,
+		imdbmetascore: 'N/A',
+		imdbplot:
+			'Ganta, the sole survivor of the brutal murder of his entire class, is framed and ends up falsely convicted for it. During the massacre the real killer embeds a red crystal in him. Ganta is sent to a privately owned prison which has a rather different and perverted take on incarceration, entertaining the masses by performing dangerous games in exchange for privileges. Every inmate he encounters has their own goals, survival being the main one, and it is now up to him to survive this and maybe even prove his innocence.',
+		imdbposter:
+			'https://images-na.ssl-images-amazon.com/images/M/MV5BYTUxNjUyNDQtYzk3ZS00NWY0LTllMDctYjQyNzBmNTIwY2M2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg',
+		imdbrated: 'TV-MA',
+		imdbrating: 7.2,
+		imdbruntime: '24 min',
+		imdbvotes: 8133,
+		img:
+			'https://occ-0-768-769.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABX8_JnjMS-G47JscmOxlavFbj5QYzV5oe_N6icZpxLfNjW0LMwcWkOxDoudko7bwYBbwJ15eSmA2wWW9tOl_ccdIzQ.jpg?r=6c5',
+		lgimg:
+			'https://occ-0-55-56.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABbypzBw91rwf3w_6ICXOv6oquOtUOQle74Gtf3M4lUFbe_zmFx75MjZXg0Ic8A3xWu8Dp1bU6DEcYGr0NwebJbEYC0Zq.jpg?r=a9f',
+		matlabel: 'Suitable for age 16 or older.',
+		matlevel: '100',
+		netflixid: 80005822,
+		netflixruntime: 0,
+		nfdate: '2015-04-14',
+		nfid: 80005822,
+		predrating: 4.8,
+		synopsis:
+			'Falsely convicted for the mass murder of his classmates, a schoolboy must endure a nightmarish penal system, all while trying to prove his innocence.',
+		title: 'Deadman Wonderland',
+		vtype: 'series',
+		year: 2011
+	},
+	{
+		avgrating: 4.2888327,
+		curdate: '2020-11-28',
+		imdbawards: '1 win & 3 nominations.',
+		imdbcountry: 'Japan',
+		imdbgenre: 'Animation, Action, Drama, Horror, Thriller',
+		imdbid: 'tt1909447',
+		imdblanguage: 'Japanese, English',
+		imdblocalimage: 1,
+		imdbmetascore: 'N/A',
+		imdbplot:
+			'Ganta, the sole survivor of the brutal murder of his entire class, is framed and ends up falsely convicted for it. During the massacre the real killer embeds a red crystal in him. Ganta is sent to a privately owned prison which has a rather different and perverted take on incarceration, entertaining the masses by performing dangerous games in exchange for privileges. Every inmate he encounters has their own goals, survival being the main one, and it is now up to him to survive this and maybe even prove his innocence.',
+		imdbposter:
+			'https://images-na.ssl-images-amazon.com/images/M/MV5BYTUxNjUyNDQtYzk3ZS00NWY0LTllMDctYjQyNzBmNTIwY2M2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg',
+		imdbrated: 'TV-MA',
+		imdbrating: 7.2,
+		imdbruntime: '24 min',
+		imdbvotes: 8133,
+		img:
+			'https://occ-0-768-769.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABX8_JnjMS-G47JscmOxlavFbj5QYzV5oe_N6icZpxLfNjW0LMwcWkOxDoudko7bwYBbwJ15eSmA2wWW9tOl_ccdIzQ.jpg?r=6c5',
+		lgimg:
+			'https://occ-0-55-56.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABbypzBw91rwf3w_6ICXOv6oquOtUOQle74Gtf3M4lUFbe_zmFx75MjZXg0Ic8A3xWu8Dp1bU6DEcYGr0NwebJbEYC0Zq.jpg?r=a9f',
+		matlabel: 'Suitable for age 16 or older.',
+		matlevel: '100',
+		netflixid: 80005822,
+		netflixruntime: 0,
+		nfdate: '2015-04-14',
+		nfid: 80005822,
+		predrating: 4.8,
+		synopsis:
+			'Falsely convicted for the mass murder of his classmates, a schoolboy must endure a nightmarish penal system, all while trying to prove his innocence.',
+		title: 'Deadman Wonderland',
+		vtype: 'series',
+		year: 2011
+	},
+	{
+		avgrating: 4.2888327,
+		curdate: '2020-11-28',
+		imdbawards: '1 win & 3 nominations.',
+		imdbcountry: 'Japan',
+		imdbgenre: 'Animation, Action, Drama, Horror, Thriller',
+		imdbid: 'tt1909447',
+		imdblanguage: 'Japanese, English',
+		imdblocalimage: 1,
+		imdbmetascore: 'N/A',
+		imdbplot:
+			'Ganta, the sole survivor of the brutal murder of his entire class, is framed and ends up falsely convicted for it. During the massacre the real killer embeds a red crystal in him. Ganta is sent to a privately owned prison which has a rather different and perverted take on incarceration, entertaining the masses by performing dangerous games in exchange for privileges. Every inmate he encounters has their own goals, survival being the main one, and it is now up to him to survive this and maybe even prove his innocence.',
+		imdbposter:
+			'https://images-na.ssl-images-amazon.com/images/M/MV5BYTUxNjUyNDQtYzk3ZS00NWY0LTllMDctYjQyNzBmNTIwY2M2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg',
+		imdbrated: 'TV-MA',
+		imdbrating: 7.2,
+		imdbruntime: '24 min',
+		imdbvotes: 8133,
+		img:
+			'https://occ-0-768-769.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABX8_JnjMS-G47JscmOxlavFbj5QYzV5oe_N6icZpxLfNjW0LMwcWkOxDoudko7bwYBbwJ15eSmA2wWW9tOl_ccdIzQ.jpg?r=6c5',
+		lgimg:
+			'https://occ-0-55-56.1.nflxso.net/dnm/api/v6/evlCitJPPCVCry0BZlEFb5-QjKc/AAAABbypzBw91rwf3w_6ICXOv6oquOtUOQle74Gtf3M4lUFbe_zmFx75MjZXg0Ic8A3xWu8Dp1bU6DEcYGr0NwebJbEYC0Zq.jpg?r=a9f',
+		matlabel: 'Suitable for age 16 or older.',
+		matlevel: '100',
+		netflixid: 80005822,
+		netflixruntime: 0,
+		nfdate: '2015-04-14',
+		nfid: 80005822,
+		predrating: 4.8,
+		synopsis:
+			'Falsely convicted for the mass murder of his classmates, a schoolboy must endure a nightmarish penal system, all while trying to prove his innocence.',
+		title: 'Deadman Wonderland',
+		vtype: 'series',
+		year: 2011
+	}
+];
