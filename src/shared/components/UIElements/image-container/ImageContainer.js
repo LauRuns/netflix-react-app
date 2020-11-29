255724d4d29a3510eb60bb624ed13b49bad5c6a5
Andy <aste.rix@icloud.com>
2020-11-29 11:45:03 +0100

    refactors image container

--- src/shared/components/UIElements/image-container/ImageContainer.js
+++ src/shared/components/UIElements/image-container/ImageContainer.js
@@ -2,9 +2,24 @@ import React from 'react';
 
 import './ImageContainer.scss';
 
-export const ImageContainer = ({ src, alt, onClick }) => {
+const defaultStyles = { display: 'inline-block', verticalAlign: 'middle' };
+
+export const ImageContainer = ({ src, alt, onClick, style, imgSize, className }) => {
+	const defaultImg = {
+		size: 32,
+		style: {},
+		className: ''
+	};
+
+	const styles = { ...defaultStyles, ...style };
 	return (
-		<div onClick={onClick} className="image-contained">
+		<div
+			onClick={onClick}
+			style={styles}
+			className={`image-contained ${className}`}
+			width={`${imgSize || defaultImg.size}px`}
+			height={`${imgSize || defaultImg.size}px`}
+		>
 			<img src={src} alt={alt} />
 		</div>
 	);
