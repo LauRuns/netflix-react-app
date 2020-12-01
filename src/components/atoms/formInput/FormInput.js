d9506b5e34eaa8faadf6aa95e45d79fe221eb971
Andy <aste.rix@icloud.com>
2020-12-01 11:26:44 +0100

    adds from input atom

---
+++ src/components/atoms/formInput/FormInput.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import './FormInput.scss';
+
+export const FormInput = ({
+	children,
+	type,
+	placeholder,
+	size,
+	name,
+	fieldRef,
+	value,
+	onChange
+}) => {
+	return (
+		<input
+			type={type || 'text'}
+			placeholder={placeholder}
+			className={`input-item input--${size}`}
+			name={name}
+			id={name}
+			ref={fieldRef}
+			value={value}
+			onChange={onChange}
+		>
+			{children}
+		</input>
+	);
+};
