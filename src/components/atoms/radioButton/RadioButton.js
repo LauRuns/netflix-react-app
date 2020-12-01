26f67ec69109a5b84197350235fe583574e6bced
Andy <aste.rix@icloud.com>
2020-12-01 11:27:15 +0100

    adds radiobutton atom

---
+++ src/components/atoms/radioButton/RadioButton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Label } from '../label/Label';
+import { FormInput } from '../formInput/FormInput';
+import './RadioButton.scss';
+
+export const RadioButton = ({ name, fieldRef, labelSize, label, value, onChange }) => {
+	return (
+		<div className="radio-button-item">
+			<FormInput type="radio" name={name} fieldRef={fieldRef} value={value} onChange={onChange} />
+			<Label name={name} size={labelSize}>
+				{label}
+			</Label>
+		</div>
+	);
+};
