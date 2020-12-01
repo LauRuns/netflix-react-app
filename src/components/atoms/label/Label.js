a0f577c1fe3e6fc6c4008822ca76f43f39e553ca
Andy <aste.rix@icloud.com>
2020-12-01 11:26:59 +0100

    adds label atom

---
+++ src/components/atoms/label/Label.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Label.scss';
+
+export const Label = ({ name, size, children }) => {
+	return (
+		<label className={`label-item label--${size}`} htmlFor={name}>
+			{children}
+		</label>
+	);
+};
