a69befddeccf0f2b843909966ac4f52eda2434ca
Andy <aste.rix@icloud.com>
2020-12-01 11:26:16 +0100

    adds indexes

---
+++ src/components/atoms/atomsIndex.js
@@ -0,0 +1,8 @@
+import { ErrorMessage } from './errorMessage/ErrorMessage';
+import { FormInput } from './formInput/FormInput';
+import { Label } from './label/Label';
+import { RadioButton } from './radioButton/RadioButton';
+import { Select } from './select/Select';
+import { SelectOption } from './selectOption/SelectOption';
+
+export { ErrorMessage, FormInput, Label, RadioButton, Select, SelectOption };
---
+++ src/components/molecules/formMolecules.js
@@ -0,0 +1,6 @@
+import { TextInput } from './textInput/TextInput';
+import { NumberInput } from './numberInput/NumberInput';
+import { RadioButtonGroup } from './radioButtonGroup/RadioButtonGroup';
+import { Dropdown } from './dropDown/Dropdown';
+
+export { TextInput, NumberInput, RadioButtonGroup, Dropdown };
