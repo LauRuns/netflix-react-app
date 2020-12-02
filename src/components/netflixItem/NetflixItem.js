7b5644dc96be5beabdb81b2b3cbb0ae33eb481e4
Andy <aste.rix@icloud.com>
2020-12-02 20:21:01 +0100

    adds conditional rendering and card animation

--- src/components/netflixItem/NetflixItem.js
+++ src/components/netflixItem/NetflixItem.js
@@ -1,37 +1,49 @@
 import React from 'react';
 
-import Card from '../../shared/components/UIElements/Card/Card';
-import { ImageContainer } from '../../shared/components/UIElements/image-container/ImageContainer';
 import './NetflixItem.scss';
 
 export const NetflixItem = ({ item }) => {
 	return (
-		<Card>
-			<div className="netflix-item__content">
-				<div>
+		<div className="flip-card">
+			<div className="flip-card-inner">
+				<div className="flip-card-front">
+					<img src={item.img} alt="Avatar" />
+				</div>
+				<div className="flip-card-back">
 					<p>{item.synopsis}</p>
 					<br />
-					<p>
-						<b>Year: </b>
-						{item.year || 'No data available'}
-					</p>
-					<p>
-						<b>IMDB rating: </b>
-						{item.imdbrating || 'No data available'}
-					</p>
-					<p>
-						<b>Language: </b>
-						{item.imdblanguage || 'No data available'}
-					</p>
-					<p>
-						<b>Genre: </b>
-						{item.imdbgenre || 'No data available'}
-					</p>
-				</div>
-				<div className="netflix-item__image">
-					<ImageContainer src={item.img} alt={item.title} />
+					{item.year && (
+						<p>
+							<b>Year: </b>
+							{item.year}
+						</p>
+					)}
+					{item.imdbrating && (
+						<p>
+							<b>IMDB rating: </b>
+							{item.imdbrating}
+						</p>
+					)}
+					{item.imdblanguage && (
+						<p>
+							<b>Language: </b>
+							{item.imdblanguage}
+						</p>
+					)}
+					{item.imdbgenre && (
+						<p>
+							<b>Genre: </b>
+							{item.imdbgenre}
+						</p>
+					)}
+					{item.nfid && (
+						<p>
+							<b>Netflix ID: </b>
+							{item.nfid}
+						</p>
+					)}
 				</div>
 			</div>
-		</Card>
+		</div>
 	);
 };
