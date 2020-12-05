4e8886986602cda24ebf8b802d727bddaad6c9c7
Andy <aste.rix@icloud.com>
2020-12-05 16:51:02 +0100

    updates imports

--- src/components/organisms/search/Search.js
+++ src/components/organisms/search/Search.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 import { Icon, Header } from '../../atoms';
-
 import './Search.scss';
 
 export const Search = React.memo(({ onLoadCountryFilter, countryList, setUpdatedCountryData }) => {
