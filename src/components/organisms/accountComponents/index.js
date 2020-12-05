33794d7d79c95df129d65932202c353bbae4141c
Andy <aste.rix@icloud.com>
2020-12-05 14:18:53 +0100

    renames indexes

---
+++ src/components/organisms/accountComponents/index.js
@@ -0,0 +1,6 @@
+import { CountrySetter } from './countrySetter/CountrySetter';
+import { PasswordChange } from './passwordChange/PasswordChange';
+import { ProfileInfo } from './profile/ProfileInfo';
+import { UploadImage } from './uploadImage/UploadImage';
+
+export { CountrySetter, PasswordChange, ProfileInfo, UploadImage };
---
+++ src/components/organisms/index.js
@@ -0,0 +1,14 @@
+import { SearchForm } from './searchForm/SearchForm';
+import { SearchFormResults } from './searchFormResults/SearchFormResults';
+import { Carousel } from './carousel/Carousel';
+import { CountrySetter, PasswordChange, ProfileInfo, UploadImage } from './accountComponents';
+
+export {
+	SearchForm,
+	SearchFormResults,
+	Carousel,
+	CountrySetter,
+	PasswordChange,
+	ProfileInfo,
+	UploadImage
+};
