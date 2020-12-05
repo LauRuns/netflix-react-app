ebe47be793b24cdf71f51492a998b2be01fb7ff9
Andy <aste.rix@icloud.com>
2020-12-05 13:57:57 +0100

    adds new carousel component

---
+++ src/components/organisms/carousel/Carousel.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import './Carousel.scss';
+/* Swiper SCSS */
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
+
+// install Swiper components
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+
+export const Carousel = ({ list, itemClicked }) => {
+	return (
+		<Swiper
+			spaceBetween={0}
+			slidesPerView={3}
+			navigation
+			pagination={{ clickable: true }}
+			scrollbar={{ draggable: true }}
+			onSlideChange={(swiper) => console.log('slide change', swiper.activeIndex)}
+			onSwiper={(swiper) => console.log(swiper)}
+		>
+			{list &&
+				list.map((item, index) => {
+					return (
+						<SwiperSlide key={index} onClick={() => itemClicked(item)}>
+							<img src={item.img} alt="poster" style={{ maxWidth: '100%', height: '100%' }} />
+						</SwiperSlide>
+					);
+				})}
+		</Swiper>
+	);
+};
