3fc2ac2b8ef8300fc9f2f43069792c42b314db3e
Andy <aste.rix@icloud.com>
2020-12-06 21:48:42 +0100

    adds context and router to top level component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { App } from './App';
 import * as serviceWorker from './serviceWorker';
+import { AuthProvider } from './shared/hooks/authentication-hook';
 
-const app = (
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
+import './index.scss';
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(
+	<React.StrictMode>
+		<Router>
+			<AuthProvider>
+				<App />
+			</AuthProvider>
+		</Router>
+	</React.StrictMode>,
+	rootElement
 );
 
-ReactDOM.render(app, document.getElementById('root'));
+// ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
