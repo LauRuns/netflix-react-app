ef0a97e9533b16b93e298137ce0185a10a3c67af
Andy <aste.rix@icloud.com>
2020-12-06 21:45:45 +0100

    adds routes component

---
+++ src/routes/Routes.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { PrivateRoute } from '../shared/auth/PrivateRoute';
+import {
+	LandingPage,
+	CountriesPage,
+	CountryDetailPage,
+	SearchPage,
+	AccountPage,
+	PageNotFound,
+	LoginPage
+} from '../pages';
+
+export const Routes = () => {
+	return (
+		<Switch>
+			<Route path="/login" exact component={LoginPage} />
+			<PrivateRoute path="/home" exact>
+				<LandingPage />
+			</PrivateRoute>
+			<PrivateRoute path="/countries" exact>
+				<CountriesPage />
+			</PrivateRoute>
+			<PrivateRoute path="/countryinfo/:countryId/:countryName" exact>
+				<CountryDetailPage />
+			</PrivateRoute>
+			<PrivateRoute path="/search" exact>
+				<SearchPage />
+			</PrivateRoute>
+			<PrivateRoute path="/account/:userId" exact>
+				<AccountPage />
+			</PrivateRoute>
+			<PrivateRoute path="*">
+				<PageNotFound />
+			</PrivateRoute>
+		</Switch>
+	);
+};
