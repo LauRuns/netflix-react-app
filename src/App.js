e9c729e6fbcd542312ed092f9497714c8f67b4ed
Andy <aste.rix@icloud.com>
2020-12-06 21:45:27 +0100

    implements new routing

--- src/App.js
+++ src/App.js
@@ -1,64 +1,83 @@
 import React from 'react';
-import {
-	Route,
-	// Redirect,
-	Switch
-} from 'react-router-dom';
 
-import {
-	AccountPage,
-	LoginPage,
-	CountryDetailPage,
-	LandingPage,
-	CountriesPage,
-	MainNavigation,
-	SearchPage
-} from './pages';
+import { MainNavigation } from './pages';
+import { Routes } from './routes/Routes';
 
-import { AuthContext } from './shared/context/auth-context';
-import useAuth from './shared/hooks/auth-hook';
+// import { AuthContext } from './shared/context/auth-context';
+// import useAuth from './shared/hooks/auth-hook';
 
 import './App.scss';
 
-const App = () => {
-	const { token, login, logout, updateCountry, userId, userCountry } = useAuth();
-	let routes;
+export const App = () => {
+	// const { token, login, logout, updateCountry, userId, userCountry } = useAuth();
+	// let routes;
 
-	if (token) {
-		routes = (
-			<Switch>
-				<Route path="/auth" exact component={LoginPage} />
-				<Route path="/countries" exact component={CountriesPage} />
-				<Route path="/account" exact component={AccountPage} />
-				<Route path="/countryinfo" exact component={CountryDetailPage} />
-				<Route path="/search" exact component={SearchPage} />
-				<Route path="/home" exact component={LandingPage} />
-			</Switch>
-		);
-	} else {
-		routes = (
-			<Switch>
-				<Route path="/auth" exact component={LoginPage} />
-			</Switch>
-		);
-	}
+	// routes = (
+	// 	<Switch>
+	// 		<Route path="/login" exact component={LoginPage} />
+	// 		<PrivateRoute path="/home" exact>
+	// 			<LandingPage />
+	// 		</PrivateRoute>
+	// 		<PrivateRoute path="/countries" exact>
+	// 			<CountriesPage />
+	// 		</PrivateRoute>
+	// 		<PrivateRoute path="/countryinfo/:countryId/:countryName" exact>
+	// 			<CountryDetailPage />
+	// 		</PrivateRoute>
+	// 		<PrivateRoute path="/search" exact>
+	// 			<SearchPage />
+	// 		</PrivateRoute>
+	// 		<PrivateRoute path="/account/:userId" exact>
+	// 			<AccountPage />
+	// 		</PrivateRoute>
+	// 		<PrivateRoute>
+	// 			<PageNotFound />
+	// 		</PrivateRoute>
+	// 	</Switch>
+	// );
+
+	// if (token) {
+	// 	routes = (
+	// 		<Switch>
+	// 			<Route path="/countries" exact component={CountriesPage} />
+	// 			<Route path="/account/:userId" exact component={AccountPage} />
+	// 			<Route path="/countryinfo/:countryId/:countryName" exact component={CountryDetailPage} />
+	// 			<Route path="/search" exact component={SearchPage} />
+	// 			<Route path="/home" exact component={LandingPage} />
+	// 			<Route component={PageNotFound} />
+	// 		</Switch>
+	// 	);
+	// } else {
+	// 	console.log('Redirecting');
+	// 	routes = (
+	// 		<Switch>
+	// 			<Route path="/login" exact component={LoginPage} />
+	// 			<Route component={PageNotFound} />
+	// 		</Switch>
+	// 	);
+	// }
 
 	return (
-		<AuthContext.Provider
-			value={{
-				isLoggedIn: !!token,
-				token: token,
-				userId: userId,
-				country: userCountry,
-				login: login,
-				logout: logout,
-				updateCountry: updateCountry
-			}}
-		>
+		<>
 			<MainNavigation />
-			<div className="content">{routes}</div>
-		</AuthContext.Provider>
+			<div className="main-content">
+				<Routes />
+			</div>
+		</>
+
+		// <AuthContext.Provider
+		// 	value={{
+		// 		isLoggedIn: !!token,
+		// 		token: token,
+		// 		userId: userId,
+		// 		country: userCountry,
+		// 		login: login,
+		// 		logout: logout,
+		// 		updateCountry: updateCountry
+		// 	}}
+		// >
+		// 	<MainNavigation />
+		// 	<div className="content">{routes}</div>
+		// </AuthContext.Provider>
 	);
 };
-
-export default App;
