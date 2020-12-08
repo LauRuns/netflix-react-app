72077969e3cf6f2a0c990cd70e7a27e2590909e6
Andy <aste.rix@icloud.com>
2020-12-08 22:04:12 +0100

    removes results section

--- src/Pages/SearchPage/SearchPage.js
+++ src/Pages/SearchPage/SearchPage.js
@@ -1,117 +1,70 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import { useHttpClient } from '../../shared/hooks/http-hook';
-import { AuthContext } from '../../shared/context/auth-context';
+import { useAuthentication } from '../../shared/hooks/authentication-hook';
 import { Header } from '../../components/atoms';
-import { NetflixItem } from '../../components/molecules';
-import { SearchForm, SearchFormResults } from '../../components/organisms';
-import { ErrorModal, LoadingSpinner, IconButton, Modal } from '../../components/uiElements';
+import { SearchForm } from '../../components/organisms';
+import { ErrorModal, LoadingSpinner } from '../../components/uiElements';
 
 // remove after development
-// import {
-// 	testCountryList,
-// 	testSearchResults,
-// 	singleSearchResult,
-// 	multipleSearchResult
-// } from '../../assets/testitems';
+import {
+	testCountryList
+	// testSearchResults,
+	// singleSearchResult,
+	// multipleSearchResult
+} from '../../assets/testitems';
 
 import './SearchPage.scss';
 
 export const SearchPage = () => {
 	const { isLoading, error, sendRequest, clearError } = useHttpClient();
-	const auth = useContext(AuthContext);
+	const { token } = useAuthentication();
+	const isMounted = useRef(null);
 
 	const [countryList, setCountryList] = useState();
-	const [searchResults, setSearchResults] = useState();
 
-	const [selectedItem, setSelectedItem] = useState(null);
-	const [showSelected, setShowSelected] = useState(false);
-
-	const loadCountries = async () => {
-		try {
-			const responseData = await sendRequest(
-				`${process.env.REACT_APP_CONNECTION_STRING}/netflix/countries`
-			);
-			const { results, count } = responseData;
-			console.log('Loaded country data____:', results);
-			console.log('Nr of loaded countries:____:', count);
-			setCountryList(results);
-		} catch (err) {
-			// Error is handled by useHttpClient
-		}
-	};
+	const history = useHistory();
 
 	useEffect(() => {
-		// setCountryList(testCountryList);
+		isMounted.current = true;
+		setCountryList(testCountryList); // <-- dev
 		// setSearchResults(testSearchResults);
 		// setSearchResults(singleSearchResult);
 		// setSearchResults(multipleSearchResult);
 
-		loadCountries();
-	}, [sendRequest]);
-
-	const searchFormSubmitHandler = async (event) => {
-		console.log('EVENT__DATA____:', event);
-		const { contentselector, countryselect, endyear, startyear, query } = event;
-
-		try {
-			const searchResponseData = await sendRequest(
-				`${process.env.REACT_APP_CONNECTION_STRING}/netflix/search`,
-				'POST',
-				JSON.stringify({
-					query: query,
-					countrylist: countryselect,
-					start_year: startyear,
-					end_year: endyear,
-					type: contentselector
-				}),
-				{
-					'Content-Type': 'application/json',
-					Authorization: `Bearer ${auth.token}`
+		const loadCountries = async () => {
+			try {
+				const responseData = await sendRequest(
+					`${process.env.REACT_APP_CONNECTION_STRING}/netflix/countries`
+				);
+				const { results } = responseData;
+				console.log('Loaded country data____:', results);
+				if (isMounted.current) {
+					setCountryList(results);
 				}
-			);
-			const { results, count } = searchResponseData;
-			console.log('Search resonse count____:', count);
-			console.log('Search response results____:', results);
-			setSearchResults(results);
-		} catch (err) {
-			// Error is handled by useHttpClient
-		}
-	};
+			} catch (err) {
+				// Error is handled by useHttpClient
+			}
+		};
 
-	const openModal = () => setShowSelected(true);
-	const closeModal = () => setShowSelected(false);
+		// loadCountries();
+		return () => {
+			isMounted.current = false;
+		};
+	}, []);
 
-	const resultItemClikedHandler = (data) => {
-		console.log('resultItemClikedHandler____:', data);
-		setSelectedItem(data);
-		openModal();
+	const fetchSearchResults = ({ contentselector, countryselect, endyear, startyear, query }) => {
+		history.push({
+			pathname: '/search/results',
+			search: `?title=${query}&start_year=${startyear}&end_year=${endyear}&content_type=${contentselector}&country=${countryselect}`
+		});
 	};
 
 	return (
 		<React.Fragment>
 			<ErrorModal error={error} onClear={clearError} />
-			{selectedItem && (
-				<Modal
-					show={showSelected}
-					header={selectedItem.title}
-					onCancel={closeModal}
-					footer={
-						<IconButton
-							icon="cancel"
-							buttonType="button"
-							before
-							inverse
-							iconStyle={{ marginRight: '0.5rem' }}
-							onClick={closeModal}
-						>
-							CLOSE
-						</IconButton>
-					}
-				>
-					<NetflixItem item={selectedItem} />
-				</Modal>
-			)}
+			{isLoading && <LoadingSpinner asOverlay loadingSpinnerMessage="Fetching countrylist..." />}
 			<div className="search-page-container">
 				<div id="search-page-header" className="search-page__header">
 					<Header lg center>
@@ -119,21 +72,7 @@ export const SearchPage = () => {
 					</Header>
 				</div>
 				<div id="search-page-form" className="search-page__form">
-					<SearchForm countries={countryList} sendFormData={searchFormSubmitHandler} />
-				</div>
-				<div id="search-page-reults" className="search-page__results">
-					{isLoading && <LoadingSpinner center loadingSpinnerMessage="Bussy fetching data..." />}
-					{!isLoading && searchResults && (
-						<SearchFormResults
-							resultData={searchResults}
-							onClick={resultItemClikedHandler}
-							header={
-								searchResults.length > 1
-									? searchResults.length + ' results:'
-									: searchResults.length + ' result:'
-							}
-						/>
-					)}
+					<SearchForm countries={countryList} sendFormData={fetchSearchResults} />
 				</div>
 			</div>
 		</React.Fragment>
