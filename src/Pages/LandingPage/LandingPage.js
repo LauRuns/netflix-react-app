ee4f1dc9992c28093977533d465cfbc2ad3b63f4
Andy <aste.rix@icloud.com>
2020-12-12 17:00:07 +0100

    sets a single loading spinner

--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -88,6 +88,7 @@ export const LandingPage = () => {
 
 	return (
 		<React.Fragment>
+			{isLoading && <LoadingSpinner asOverlay loadingSpinnerMessage="Fetching data..." />}
 			<ErrorModal error={error} onClear={clearError} />
 			{selectedItem && (
 				<Modal
@@ -110,8 +111,6 @@ export const LandingPage = () => {
 					<NetflixItem item={selectedItem} />
 				</Modal>
 			)}
-			{/* {isLoading && <LoadingSpinner asOverlay loadingSpinnerMessage="Fetching data..." />} */}
-
 			<div className="landingpage-container">
 				<div id="homepage-header" className="homepage__header">
 					<Header lg>
@@ -121,64 +120,40 @@ export const LandingPage = () => {
 						<h3>Here is your new &amp; expiring data for {currentUserCountry.country}</h3>
 					)}
 				</div>
-				<div id="homepage-expiring" className="homepage__expiring">
-					<Header md>
-						{currentUserCountry && <h2>Expiring content for {currentUserCountry.country}:</h2>}
-					</Header>
-					{isLoading ? (
-						<LoadingSpinner
-							center
-							loadingSpinnerMessage={`Loading data for ${currentUserCountry.country}`}
-						/>
-					) : (
-						expOther && <Slider slideList={expOther} onClick={onItemClickedHandler} />
-					)}
-				</div>
-				<div id="homepage-nld-expiring" className="homepage__nld__expiring">
-					<Header md>
-						<h2>Netherlands expiring content:</h2>
-					</Header>
-					{isLoading ? (
-						<LoadingSpinner center loadingSpinnerMessage={`Loading expiring content...`} />
-					) : (
-						expNLD && <Slider slideList={expNLD} onClick={onItemClickedHandler} />
-					)}
-				</div>
-				<div id="homepage-nld-new-content" className="homepage__nld__new_content">
-					<Header md>
-						<h2>New content for the Netherlands:</h2>
-					</Header>
-					{isLoading ? (
-						<LoadingSpinner center loadingSpinnerMessage={`Loading new content...`} />
-					) : (
-						nldNewContent && <Slider slideList={nldNewContent} onClick={onItemClickedHandler} />
-					)}
-				</div>
-				<div id="homepage-testing-carousel" className="homepage__testing__carousel">
-					<Header md>
-						{currentUserCountry && <h2>New content for {currentUserCountry.country}:</h2>}
-					</Header>
-					{/* {isLoading ? (
-						<LoadingSpinner
-							center
-							loadingSpinnerMessage={`Loading new content for ${userCountry.country}`}
-						/>
-					) : (
-						otherNewContent && (
-							<SampleSlider slideList={otherNewContent} onClick={onItemClickedHandler} />
-						)
-					)} */}
-					{isLoading ? (
-						<LoadingSpinner
-							center
-							loadingSpinnerMessage={`Loading new content for ${currentUserCountry.country}`}
-						/>
-					) : (
-						otherNewContent && (
+				{!isLoading && (
+					<div id="homepage-expiring" className="homepage__expiring">
+						<Header md>
+							{currentUserCountry && <h2>Expiring content for {currentUserCountry.country}:</h2>}
+						</Header>
+						{expOther && <Slider slideList={expOther} onClick={onItemClickedHandler} />}
+					</div>
+				)}
+				{!isLoading && (
+					<div id="homepage-nld-expiring" className="homepage__nld__expiring">
+						<Header md>
+							<h2>Netherlands expiring content:</h2>
+						</Header>
+						{expNLD && <Slider slideList={expNLD} onClick={onItemClickedHandler} />}
+					</div>
+				)}
+				{!isLoading && (
+					<div id="homepage-nld-new-content" className="homepage__nld__new_content">
+						<Header md>
+							<h2>New content for the Netherlands:</h2>
+						</Header>
+						{nldNewContent && <Slider slideList={nldNewContent} onClick={onItemClickedHandler} />}
+					</div>
+				)}
+				{!isLoading && (
+					<div id="homepage-testing-carousel" className="homepage__testing__carousel">
+						<Header md>
+							{currentUserCountry && <h2>New content for {currentUserCountry.country}:</h2>}
+						</Header>
+						{otherNewContent && (
 							<Carousel list={otherNewContent} itemClicked={onItemClickedHandler} />
-						)
-					)}
-				</div>
+						)}
+					</div>
+				)}
 			</div>
 		</React.Fragment>
 	);
