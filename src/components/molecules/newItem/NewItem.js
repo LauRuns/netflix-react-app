cc4805f3fa4702bdc230b1b77e516dd41c38ad5b
Andy <aste.rix@icloud.com>
2020-12-15 00:21:34 +0100

    adds new component

---
+++ src/components/molecules/newItem/NewItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Card, ImageContainer } from '../../uiElements';
+
+import './NewItem.scss';
+
+export const NewItem = ({ item, itemClick }) => {
+	return (
+		<>
+			<Card onClick={itemClick} cardStyles={{ display: 'inline-block', margin: '0.5rem' }}>
+				<ImageContainer src={item?.img} />
+			</Card>
+		</>
+	);
+};
