b1cb790ebea420c8d28ca7e71c1b7ed66fe02a89
Andy <aste.rix@icloud.com>
2020-12-14 23:35:05 +0100

    adds two new components

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -4,5 +4,16 @@ import { RadioButtonGroup } from './radioButtonGroup/RadioButtonGroup';
 import { Dropdown } from './dropDown/Dropdown';
 import { NetflixItem } from './netflixItem/NetflixItem';
 import { Select } from './select/Select';
+import { NavButtons } from './navButtons/NavButtons';
+import { ExpItem } from './expItem/ExpItem';
 
-export { TextInput, NumberInput, RadioButtonGroup, Dropdown, NetflixItem, Select };
+export {
+	TextInput,
+	NumberInput,
+	RadioButtonGroup,
+	Dropdown,
+	NetflixItem,
+	Select,
+	NavButtons,
+	ExpItem
+};
