0961267f4310e275403f4f4712cffab54bee003c
Andy <aste.rix@icloud.com>
2020-12-15 11:11:18 +0100

    updates layout

--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-import { useHttpClient } from '../../shared/hooks/http-hook';
-import { IconButton, Modal, ErrorModal, LoadingSpinner } from '../../components/uiElements';
+import { IconButton, Modal } from '../../components/uiElements';
 import { Header } from '../../components/atoms';
 import { NetflixItem } from '../../components/molecules';
 import { ExpContentList, NewContentList } from '../../components/organisms';
@@ -9,8 +8,6 @@ import { ExpContentList, NewContentList } from '../../components/organisms';
 import './LandingPage.scss';
 
 export const LandingPage = () => {
-	const { isLoading, error, sendRequest, clearError } = useHttpClient();
-
 	const [currentUserCountry, setCurrentUserCountry] = useState(null);
 	const [selectedItem, setSelectedItem] = useState(null);
 	const [showSelected, setShowSelected] = useState(false);
@@ -21,7 +18,6 @@ export const LandingPage = () => {
 	}, []);
 
 	const onItemClickedHandler = (data) => {
-		console.log('onItemClickedHandler', data);
 		setSelectedItem(data);
 		openModal();
 	};
@@ -31,8 +27,6 @@ export const LandingPage = () => {
 
 	return (
 		<React.Fragment>
-			{isLoading && <LoadingSpinner asOverlay loadingSpinnerMessage="Fetching data..." />}
-			<ErrorModal error={error} onClear={clearError} />
 			{selectedItem && (
 				<Modal
 					show={showSelected}
@@ -63,8 +57,24 @@ export const LandingPage = () => {
 						<h3>Here is your new &amp; expiring data for {currentUserCountry.country}</h3>
 					)}
 				</div>
-
-				<div id="homepage-expiring" className="homepage__expiring">
+				<div id="homepage-new-current" className="homepage__new__current">
+					<Header md>
+						{currentUserCountry && <h2>New content for {currentUserCountry.country}:</h2>}
+					</Header>
+					{currentUserCountry && (
+						<NewContentList
+							countryIdCode={`${currentUserCountry.countryId}`}
+							itemClick={onItemClickedHandler}
+						/>
+					)}
+				</div>
+				<div id="homepage-nld-new" className="homepage__nld__new_content">
+					<Header md>
+						<h2>New content for the Netherlands:</h2>
+					</Header>
+					<NewContentList countryIdCode="67" itemClick={onItemClickedHandler} />
+				</div>
+				<div id="homepage-current-expiring" className="homepage__current__expiring">
 					<Header md>
 						{currentUserCountry && <h2>Expiring content for {currentUserCountry.country}:</h2>}
 					</Header>
@@ -75,31 +85,11 @@ export const LandingPage = () => {
 						/>
 					)}
 				</div>
-
 				<div id="homepage-nld-expiring" className="homepage__nld__expiring">
 					<Header md>
 						<h2>Netherlands expiring content:</h2>
 					</Header>
 					<ExpContentList countryIdCode="67" itemClick={onItemClickedHandler} />
-				</div>
-
-				<div id="homepage-nld-new-content" className="homepage__nld__new_content">
-					<Header md>
-						<h2>New content for the Netherlands:</h2>
-					</Header>
-					<NewContentList countryIdCode="67" itemClick={onItemClickedHandler} />
-				</div>
-
-				<div id="homepage-testing-carousel" className="homepage__testing__carousel">
-					<Header md>
-						{currentUserCountry && <h2>New content for {currentUserCountry.country}:</h2>}
-					</Header>
-					{currentUserCountry && (
-						<NewContentList
-							countryIdCode={`${currentUserCountry.countryId}`}
-							itemClick={onItemClickedHandler}
-						/>
-					)}
 				</div>
 			</div>
 		</React.Fragment>
