82970c57ea5bc0b09cd794aec4dd473707016927
Andy <aste.rix@icloud.com>
2020-12-19 17:35:34 +0100

    implements form validation and styling

--- src/components/formElements/countryDropdown/CountryDropdown.js
+++ src/components/formElements/countryDropdown/CountryDropdown.js
@@ -1,42 +1,97 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer, useEffect } from 'react';
+import { validate } from '../../../shared/util/validators';
 
 import { Backdrop } from '../../uiElements';
 
 import './CountryDropdown.scss';
 
-export const CountryDropdown = ({ title, label, items, selected }) => {
+const selectionReducer = (state, action) => {
+	switch (action.type) {
+		case 'CLICKED':
+			return {
+				...state,
+				value: action.val,
+				isValid: validate(action.val, action.validators)
+			};
+		case 'TOUCH': {
+			return {
+				...state,
+				isTouched: true
+			};
+		}
+		default:
+			return state;
+	}
+};
+
+export const CountryDropdown = ({
+	title,
+	items,
+	initialValue,
+	initialValid,
+	onInput,
+	id,
+	label,
+	validators,
+	errorText
+}) => {
+	const [selectionState, dispatch] = useReducer(selectionReducer, {
+		value: initialValue || null,
+		isTouched: false,
+		isValid: initialValid || false
+	});
 	const [open, setOpen] = useState(false);
-	const [selection, setSelection] = useState(null);
 	const [showBackDrop, setShowBackDrop] = useState(false);
+
+	const { value, isValid } = selectionState;
+
+	useEffect(() => {
+		onInput(id, value, isValid);
+	}, [id, value, isValid, onInput]);
+
+	const onChangeHandler = (selectedCountry) => {
+		console.log(selectedCountry);
+		dispatch({
+			type: 'CLICKED',
+			val: selectedCountry,
+			validators: validators
+		});
+		toggle();
+	};
+
+	const touchHandler = () => {
+		dispatch({
+			type: 'TOUCH'
+		});
+	};
 
 	const toggle = () => {
 		setShowBackDrop(!showBackDrop);
 		setOpen(!open);
-	};
-
-	const handleOnClick = (item) => {
-		selected(item);
-		setSelection(item.country);
-		toggle();
+		touchHandler();
 	};
 
 	return (
 		<>
 			{showBackDrop && <Backdrop onClick={toggle} />}
-			<div className="dd-container">
+			<div
+				className={`dd-container ${
+					!selectionState.isValid && selectionState.isTouched && 'dropdown-state--invalid'
+				}`}
+			>
 				<div className="dd-label">
-					<label htmlFor="dd-wrpr">{label}</label>
+					<label htmlFor={id}>{label}</label>
 				</div>
-				<div id="dd-wrpr" className="dd-wrapper">
+				<div id={id} className="dd-wrapper">
 					<div
 						tabIndex={0}
 						className="dd-header"
 						role="button"
-						onKeyPress={() => toggle(!open)}
-						onClick={() => toggle(!open)}
+						onKeyPress={toggle}
+						onClick={toggle}
 					>
-						<div className="dd-header__title">
-							<p className="dd-header__title--bold">{selection ? selection : title}</p>
+						<div className={`${selectionState.value?.country && 'dd-header__title'}`}>
+							<p>{value ? value.country : title}</p>
 						</div>
 						<div className="dd-header__action">
 							<p>{open ? 'Close' : 'Open'}</p>
@@ -46,7 +101,7 @@ export const CountryDropdown = ({ title, label, items, selected }) => {
 						<ul className="dd-list">
 							{items.map((item) => (
 								<li className="dd-list-item" key={item.countryId}>
-									<button type="button" onClick={() => handleOnClick(item)}>
+									<button type="button" onClick={() => onChangeHandler(item)}>
 										<span>{item.country}</span>
 									</button>
 								</li>
@@ -54,6 +109,9 @@ export const CountryDropdown = ({ title, label, items, selected }) => {
 						</ul>
 					)}
 				</div>
+				{!selectionState.value && selectionState.isTouched && (
+					<p id="dropdown-errortext">{errorText}</p>
+				)}
 			</div>
 		</>
 	);
