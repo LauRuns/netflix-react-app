fdc5946d32fda0d324c4937fa67ebb7fa1bf4ebe
Andy <aste.rix@icloud.com>
2020-12-19 17:35:49 +0100

    adds extra validator for country object

--- src/shared/util/validators.js
+++ src/shared/util/validators.js
@@ -5,6 +5,7 @@ const VALIDATOR_TYPE_MIN = 'MIN';
 const VALIDATOR_TYPE_MAX = 'MAX';
 const VALIDATOR_TYPE_EMAIL = 'EMAIL';
 const VALIDATOR_TYPE_FILE = 'FILE';
+const VALIDATOR_TYPE_OBJECT = 'OBJECT';
 
 export const VALIDATOR_REQUIRE = () => ({ type: VALIDATOR_TYPE_REQUIRE });
 export const VALIDATOR_FILE = () => ({ type: VALIDATOR_TYPE_FILE });
@@ -23,6 +24,9 @@ export const VALIDATOR_MAX = (val) => ({
 });
 export const VALIDATOR_EMAIL = () => ({
 	type: VALIDATOR_TYPE_EMAIL
+});
+export const VALIDATOR_OBJECT = () => ({
+	type: VALIDATOR_TYPE_OBJECT
 });
 
 export const validate = (value, validators) => {
@@ -46,6 +50,9 @@ export const validate = (value, validators) => {
 		}
 		if (validator.type === VALIDATOR_TYPE_EMAIL) {
 			isValid = isValid && /^\S+@\S+\.\S+$/.test(value);
+		}
+		if (validator.type === VALIDATOR_TYPE_OBJECT) {
+			isValid = isValid && typeof value === 'object';
 		}
 	}
 	return isValid;
