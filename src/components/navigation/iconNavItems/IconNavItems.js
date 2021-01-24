c52e7576376718f2dd1fe86f0fe8dabf4da5d6bc
Andy <aste.rix@icloud.com>
2021-01-24 12:07:25 +0100

    use new authcontext

--- src/components/navigation/iconNavItems/IconNavItems.js
+++ src/components/navigation/iconNavItems/IconNavItems.js
@@ -1,13 +1,13 @@
 import React from 'react';
 /* Hook - context */
-import { useAuthentication } from '../../../shared/hooks/authentication-hook';
+import { useAuthState } from '../../../shared/context/auth-context';
 /* Component and styling */
 import { IconNavItem } from '../iconNavItem/IconNavItem';
 import './IconNavItems.scss';
 
 /* Returns a list of IconNavItem for using in the SideDrawer */
 export const IconNavItems = ({ navItems }) => {
-	const { isAuthenticated } = useAuthentication();
+	const { isAuthenticated } = useAuthState();
 
 	return (
 		<ul className="icon-nav-items-container">
