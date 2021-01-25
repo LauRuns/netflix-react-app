924f990f89b052c40e25168c4f41ee51890c3236
Andy <aste.rix@icloud.com>
2021-01-25 22:20:45 +0100

    adds snackbar on landingpage after login

--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 /* UI elements and components */
 import { IconButton, Modal } from '../../components/uiElements';
 import { Header } from '../../components/atoms';
 import { NetflixItem } from '../../components/molecules';
 import { ExpContentList, NewContentList } from '../../components/organisms';
+import { useSnackbar } from 'react-simple-snackbar';
 /* Styling */
 import './LandingPage.scss';
 
@@ -16,6 +17,32 @@ Default data for the Netherlands will always be loaded.
 export const LandingPage = () => {
 	const [selectedItem, setSelectedItem] = useState(null);
 	const { country, countryId } = useParams();
+
+	const options = {
+		position: 'bottom-right',
+		style: {
+			backgroundColor: '#132743',
+			border: '1px solid #DB1D4B',
+			color: '#DB1D4B',
+			fontFamily: 'Menlo, monospace',
+			fontSize: '14px',
+			textAlign: 'center'
+		},
+		closeStyle: {
+			color: 'lightcoral',
+			fontSize: '16px'
+		}
+	};
+	const [openSnackbar, closeSnackbar] = useSnackbar(options);
+
+	useEffect(() => {
+		openSnackbar(
+			<p>
+				This webpage uses cookies. By using this webpage you <strong>consent</strong> to the use of
+				cookies.
+			</p>
+		);
+	}, []);
 
 	/* Open the modal presenting the selected item data */
 	const onItemClickedHandler = (data) => {
@@ -59,23 +86,23 @@ export const LandingPage = () => {
 					</div>
 					<div id="homepage-new-current" className="homepage__new__current">
 						<Header md>{country && <h2>New content for {country}:</h2>}</Header>
-						<NewContentList countryIdCode={countryId} itemClick={onItemClickedHandler} />
+						{/* <NewContentList countryIdCode={countryId} itemClick={onItemClickedHandler} /> */}
 					</div>
 					<div id="homepage-nld-new" className="homepage__nld__new_content">
 						<Header md>
 							<h2>New content for the Netherlands:</h2>
 						</Header>
-						<NewContentList countryIdCode="67" itemClick={onItemClickedHandler} />
+						{/* <NewContentList countryIdCode="67" itemClick={onItemClickedHandler} /> */}
 					</div>
 					<div id="homepage-current-expiring" className="homepage__current__expiring">
 						<Header md>{country && <h2>Expiring content for {country}:</h2>}</Header>
-						<ExpContentList countryIdCode={countryId} itemClick={onItemClickedHandler} />
+						{/* <ExpContentList countryIdCode={countryId} itemClick={onItemClickedHandler} /> */}
 					</div>
 					<div id="homepage-nld-expiring" className="homepage__nld__expiring">
 						<Header md>
 							<h2>Netherlands expiring content:</h2>
 						</Header>
-						<ExpContentList countryIdCode="67" itemClick={onItemClickedHandler} />
+						{/* <ExpContentList countryIdCode="67" itemClick={onItemClickedHandler} /> */}
 					</div>
 				</div>
 			) : (
