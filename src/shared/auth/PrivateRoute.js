f4f297dda82750f23159270feaac280cbf539edc
Andy <aste.rix@icloud.com>
2021-01-25 22:21:22 +0100

    cheks authentication based on cookie

--- src/shared/auth/PrivateRoute.js
+++ src/shared/auth/PrivateRoute.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-/* Hook/context imports */
-import { useAuthState } from '../context/auth-context';
+import { useCookies } from 'react-cookie';
+
 /*
 Checks if the user is authenticated using the useAuthState context.
 If the user !authenticated then redirect to the /login page
 If the user is authenticated then continue to the requested page
 */
 export const PrivateRoute = ({ children, ...rest }) => {
-	const { isAuthenticated } = useAuthState();
+	const [cookies] = useCookies(['token', 'userId', 'expDate']);
+
 	return (
 		<Route
 			{...rest}
 			render={({ location }) =>
-				isAuthenticated ? (
+				cookies.token ? (
 					children
 				) : (
 					<Redirect
