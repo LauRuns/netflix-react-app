d48f5225ec7a424c8646f5bee4d0065847b325e9
Andy <aste.rix@icloud.com>
2021-02-09 18:03:03 +0100

    adds check if error on img

--- src/components/uiElements/avatar/Avatar.js
+++ src/components/uiElements/avatar/Avatar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import defaultAvatar from '../../../assets/no-profile-picture.jpg';
 import './Avatar.scss';
 
@@ -7,16 +7,18 @@ Returns a container for the user image, returning a circle shaped image.
 If the user has no image set, then a default image is used as a fallback.
 */
 export const Avatar = (props) => {
-	const onImgErrorHandler = () => {
-		console.error('User Avatar has an error');
+	const [loadedImg, setLoadedImg] = useState({ imageStatus: 'loading', error: false });
+
+	const handleImageError = () => {
+		setLoadedImg({ imageStatus: 'failed to load', error: true });
 	};
 
 	return (
 		<div className={`avatar ${props.className}`} style={props.style}>
 			<img
-				onError={onImgErrorHandler}
-				src={props.image ? `${process.env.REACT_APP_ASSET_URL}/${props.image}` : defaultAvatar}
-				alt={props.alt}
+				src={loadedImg.error ? defaultAvatar : `${process.env.REACT_APP_ASSET_URL}/${props.image}`}
+				onError={handleImageError}
+				alt={loadedImg.imageStatus}
 				style={{ width: props.width, height: props.height }}
 			/>
 		</div>
